f382dd41f8f8ddb9796973ee9771cc7dd68a8653
Bowen Sun <bowensun@gmail.com>
2016-07-19 17:54:15 +0100

    fix gunicorn bind address

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
 python /usr/src/app/manage.py migrate
-gunicorn -c /usr/src/app/gunicorn/conf.py data.wsgi --log-file -
+gunicorn -c /usr/src/app/gunicorn/conf.py data.wsgi --log-file - -b [::1]:8000
 
