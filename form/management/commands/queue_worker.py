c4545c5c1fa74aa8035e444f8e864df3f0be26a5
konradko <konrad.korzel@gmail.com>
2016-09-16 05:40:41 +0100

    Add setting up logging for queue worker command

--- form/management/commands/queue_worker.py
+++ form/management/commands/queue_worker.py
@@ -1,12 +1,32 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 import form.queue
 
 
 class Command(BaseCommand):
-    help = 'Runs a queue worker'
+    help = 'Runs a form data SQS queue worker'
+
+    @staticmethod
+    def setup_logging():
+        logger = logging.getLogger()
+        console = logging.StreamHandler()
+        console.setLevel(logging.DEBUG)
+        console.setFormatter(logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        ))
+        logger.addHandler(console)
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Starting queue worker'))
+        self.setup_logging()
+
+        self.stdout.write(
+            self.style.SUCCESS('Starting queue worker')
+        )
+
         form.queue.Worker().run()
-        self.stdout.write(self.style.SUCCESS('Queue worker finished running'))
+
+        self.stdout.write(
+            self.style.SUCCESS('Queue worker finished running')
+        )
