748cf0ef60c9284367beed9def679f8f513edf43
konradko <konrad.korzel@gmail.com>
2016-10-12 03:40:17 +0100

    Add collectstatic to docker

--- docker-cmd-webserver.sh
+++ docker-cmd-webserver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
 python /usr/src/app/manage.py migrate
+python /usr/src/app/manage.py collectstatic --noinput
 gunicorn -c /usr/src/app/gunicorn/conf.py api.wsgi --bind 0.0.0.0:$PORT --log-file -
