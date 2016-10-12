8901ddfcb921ad65de1699f0e01c4851eafad75a
konradko <konrad.korzel@gmail.com>
2016-10-12 02:06:46 +0100

    Fix enrollment view, add alice signature verification

--- enrollment/views.py
+++ enrollment/views.py
@@ -1,32 +1,36 @@
 import json
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView
 from rest_framework import status
 from rest_framework.response import Response
 
+from alice.utils import SignatureRejection
 import enrollment.queue
 from enrollment.serializers import EnrollmentSerializer
 from enrollment.models import Enrollment
 
 
-class EnrollmentViewSet(ModelViewSet):
+class EnrollmentCreateAPIView(CreateAPIView):
 
     model = Enrollment
     serializer_class = EnrollmentSerializer
     http_method_names = ("post", )
 
     def __init__(self, *args, **kwargs):
-        super(EnrollmentViewSet, self).__init__(*args, **kwargs)
+        super(EnrollmentCreateAPIView, self).__init__(*args, **kwargs)
         self.enrollment_queue = enrollment.queue.Enrollment()
 
     def create(self, request, *args, **kwargs):
         """Sends valid request data to enrollment SQS queue"""
 
+        if not SignatureRejection.test_signature(request):
+            return False
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         self.enrollment_queue.send(
-            data=json.dumps(request.data.dict(), ensure_ascii=False)
+            data=json.dumps(request.data, ensure_ascii=False)
         )
 
         return Response(
