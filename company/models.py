275200c1cac2b5cb50a1c7b10a86b380202915f9
Monika Sulik <kontakt@monikasulik.net>
2016-10-13 22:14:33 +0100

    Allow empty values for company fields

--- company/models.py
+++ company/models.py
@@ -5,10 +5,10 @@ from django.contrib.postgres.fields import JSONField
 
 class Company(models.Model):
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     number = models.CharField(max_length=8, validators=[MinValueValidator(8)])
-    website = models.URLField(max_length=255)
-    description = models.TextField()
+    website = models.URLField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
     aims = JSONField()
 
     def __str__(self):
