04506da5263e8681d0b0f17c5a45a9c3e16d47bb
Monika Sulik <kontakt@monikasulik.net>
2016-10-14 17:26:17 +0100

    Pep8 correction

--- company/models.py
+++ company/models.py
@@ -12,7 +12,8 @@ class Company(models.Model):
     # specifying it as None
     name = models.CharField(max_length=255, blank=True, null=True, default='')
     number = models.CharField(max_length=8, validators=[MinLengthValidator(8)])
-    website = models.URLField(max_length=255, blank=True, null=True, default='')
+    website = models.URLField(max_length=255, blank=True, null=True,
+                              default='')
     description = models.TextField(blank=True, null=True, default='')
     aims = JSONField()
     logo = models.FileField(blank=True, null=True)
