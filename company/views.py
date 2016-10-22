7fcd2018596d780bf43688bd804a07b0944567f3
Rich Tier <rikatee@gmail.com>
2016-10-22 15:44:22 +0100

    Fetch company name from companies house

--- company/views.py
+++ company/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 
+from django.http import HttpResponse
+
+from company import helpers
 from company.models import Company
 from company.serializers import (
     CompanyNumberValidatorSerializer,
@@ -16,6 +19,22 @@ class CompanyNumberValidatorAPIView(GenericAPIView):
         validator = self.get_serializer(data=request.GET)
         validator.is_valid(raise_exception=True)
         return Response()
+
+
+class CompaniesHouseProfileDetailsAPIView(GenericAPIView):
+
+    serializer_class = CompanyNumberValidatorSerializer
+
+    def get(self, request, *args, **kwargs):
+        validator = self.get_serializer(data=request.GET)
+        validator.is_valid(raise_exception=True)
+        number = validator.validated_data['number']
+        profile_response = helpers.get_companies_house_profile(number)
+        return HttpResponse(
+            profile_response.text,
+            status=profile_response.status_code,
+            content_type='application/json'
+        )
 
 
 class CompanyRetrieveUpdateAPIView(RetrieveUpdateAPIView):
