d890232034840181601a2636072b08e2602d8992
Rich Tier <rikatee@gmail.com>
2016-10-27 12:16:09 +0100

    Add tests for confirmation email

---
+++ api/tests/test_templates.py
@@ -0,0 +1,10 @@
+from django.template.loader import render_to_string
+
+
+
+def test_confirmation_email():
+    context = {
+        'confirmation_url': 'http://confirm.com'
+    }
+    html = render_to_string('confirmation_email.html', context)
+    assert 'http://confirm.com' in html
