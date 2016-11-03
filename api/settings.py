635639adb85b79a3e8cd3eabfcaa97803cf32a1d
konradko <konrad.korzel@gmail.com>
2016-11-03 20:33:07 +0000

    Remove AWS keys from env vars

--- api/settings.py
+++ api/settings.py
@@ -199,8 +199,6 @@ GOV_NOTIFY_SERVICE_VERIFICATION_TEMPLATE_NAME = os.environ[
 
 # Public storage for company profile logo
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_DEFAULT_ACL = 'public-read'
 AWS_AUTO_CREATE_BUCKET = True
