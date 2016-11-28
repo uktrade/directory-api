9954437357104f2516db6655d0f6cd0dacf28bc3
Rich Tier <rikatee@gmail.com>
2016-11-28 16:51:04 +0000

    Test sector filtering

--- company/filters.py
+++ company/filters.py
@@ -4,14 +4,15 @@ from company import models
 
 
 class JSONFilter(django_filters.CharFilter):
-	def filter(self, qs, value):
-		if value:
-			value = '"{value}"'.format(value=value)
-		return super().filter(qs, value)
+    def filter(self, qs, value):
+        if value:
+            value = '"{value}"'.format(value=value)
+        return super().filter(qs, value)
 
 
 class CompanyPublicProfileFilter(django_filters.rest_framework.FilterSet):
     sectors = JSONFilter(name="sectors", lookup_expr='contains')
+
     class Meta:
         model = models.Company
         fields = ['sectors']
