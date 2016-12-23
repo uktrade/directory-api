b27a97776f6dbb9d20ab5bf3dfdd74d289925e50
Monika Sulik <kontakt@monikasulik.net>
2016-12-23 16:38:43 +0000

    Publishing companies via admin

--- company/admin.py
+++ company/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
 from django.conf.urls import url
-from django.http import HttpResponse
+from django.views.generic import FormView
+from django.core.urlresolvers import reverse_lazy
+from django import forms
 
 from company.models import Company, CompanyCaseStudy
+
+
+class CHNumbersForm(forms.Form):
+    COMPANY_DOESNT_EXIST_MSG = (
+        'Some companies in this data set are not in the db'
+    )
+
+    company_numbers = forms.CharField(
+        widget=forms.Textarea,
+        help_text='Comma-separated company house numbers'
+    )
+
+    def clean_company_numbers(self):
+        ch_numbers = self.cleaned_data['company_numbers'].split(',')
+        ch_numbers = [num.strip() for num in ch_numbers if num.strip()]
+
+        number_of_companies = Company.objects.filter(
+            number__in=ch_numbers).count()
+        if number_of_companies != len(ch_numbers):
+            raise forms.ValidationError(self.COMPANY_DOESNT_EXIST_MSG)
+
+        return ch_numbers
+
+
+class PublishByCHNumberView(FormView):
+    form_class = CHNumbersForm
+    template_name = 'admin_company_publish_form.html'
+    success_url = reverse_lazy('admin:company_company_changelist')
+
+    def get_context_data(self, **kwargs):
+        context = super(PublishByCHNumberView, self).get_context_data(**kwargs)
+        context['title'] = 'Publish Companies'
+        context['opts'] = Company._meta
+        return context
+
+    def form_valid(self, form):
+        numbers = form.cleaned_data['company_numbers']
+        Company.objects.filter(number__in=numbers).update(is_published=True)
+        return super(PublishByCHNumberView, self).form_valid(form)
 
 
 @admin.register(Company)
@@ -12,16 +53,11 @@ class CompanyAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(CompanyAdmin, self).get_urls()
         additional_urls = [
-            url(r'^publish/$', self.admin_site.admin_view(self.publish),
+            url(r'^publish/$',
+                self.admin_site.admin_view(PublishByCHNumberView.as_view()),
                 name="company_company_publish"),
         ]
         return additional_urls + urls
-
-    def publish(self, request):
-        """Publish companies with the numbers defined in the POST request"""
-        numbers = request.POST['company_numbers'].split(',')
-        Company.objects.filter(number__in=numbers).update(is_published=True)
-        return HttpResponse()
 
 
 @admin.register(CompanyCaseStudy)
--- company/tests/test_admin.py
+++ company/tests/test_admin.py
@@ -8,15 +8,17 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 from company.models import Company
+from company.admin import CHNumbersForm
 from company.tests import CompanyFactory
+
+
+COMPANY_DOESNT_EXIST_MSG = (
+    'Some companies in this data set are not in the db'
+)
 
 
 @pytest.mark.django_db
 class PublishCompaniesInCSVTestCase(TestCase):
-    # TODO: Test for bad data (whitespace, different format etc.)
-    # TODO: GET view call to actually input data
-    # TODO: Redirect after POST (try generic view instead of admin method)
-    # TODO: Form validation for company nums that don't exist
 
     def setUp(self):
         superuser = User.objects.create_superuser(
@@ -25,7 +27,12 @@ class PublishCompaniesInCSVTestCase(TestCase):
         self.client = Client()
         self.client.force_login(superuser)
 
-    def test_uploaded_companies_set_to_published(self):
+    def test_superuser_can_access_company_publish_view_get(self):
+        response = self.client.get(reverse('admin:company_company_publish'))
+
+        assert response.status_code == http.client.OK
+
+    def test_companies_in_post_set_to_published(self):
         companies = CompanyFactory.create_batch(5, is_published=False)
         published_company = CompanyFactory(is_published=True)
         numbers = '{num1},{num2}'.format(
@@ -36,7 +43,8 @@ class PublishCompaniesInCSVTestCase(TestCase):
             {'company_numbers': numbers},
         )
 
-        assert response.status_code == http.client.OK
+        assert response.status_code == http.client.FOUND
+        assert response.url == reverse('admin:company_company_changelist')
 
         published = Company.objects.filter(is_published=True).values_list(
             'number', flat=True)
@@ -58,28 +66,84 @@ class CompanyAdminAuthTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.company = CompanyFactory()
 
-    def test_nonsuperuser_cannot_access_company_publish_view(self):
+    def test_nonsuperuser_cannot_access_company_publish_view_get(self):
         user = User.objects.create_user(
-            username='admin', email='admin@example.com', password='test'
+            username='user', email='user@example.com', password='test'
+        )
+        self.client.force_login(user)
+        url = reverse('admin:company_company_publish')
+
+        response = self.client.get(url)
+
+        assert response.status_code == http.client.FOUND
+        assert response.url == '/admin/login/?next={redirect_to}'.format(
+            redirect_to=url)
+
+    def test_nonsuperuser_cannot_access_company_publish_view_post(self):
+        company = CompanyFactory()
+        user = User.objects.create_user(
+            username='user', email='user@example.com', password='test'
         )
         self.client.force_login(user)
         url = reverse('admin:company_company_publish')
 
         response = self.client.post(
-            url, {'company_numbers': self.company.number})
+            url, {'company_numbers': company.number})
 
         assert response.status_code == http.client.FOUND
         assert response.url == '/admin/login/?next={redirect_to}'.format(
             redirect_to=url)
 
-    def test_guest_cannot_access_company_publish_view(self):
+    def test_guest_cannot_access_company_publish_view_get(self):
         url = reverse('admin:company_company_publish')
 
-        response = self.client.post(
-            url, {'company_numbers': self.company.number})
+        response = self.client.get(url)
 
         assert response.status_code == http.client.FOUND
         assert response.url == '/admin/login/?next={redirect_to}'.format(
             redirect_to=url)
+
+    def test_guest_cannot_access_company_publish_view_post(self):
+        url = reverse('admin:company_company_publish')
+        company = CompanyFactory()
+
+        response = self.client.post(
+            url, {'company_numbers': company.number})
+
+        assert response.status_code == http.client.FOUND
+        assert response.url == '/admin/login/?next={redirect_to}'.format(
+            redirect_to=url)
+
+
+@pytest.mark.django_db
+def test_companies_publish_form_doesnt_allow_numbers_that_dont_exist():
+    # all don't exist
+    data = {
+        'company_numbers': '12345678,23456789,34567890'
+    }
+    form = CHNumbersForm(data=data)
+
+    assert form.is_valid() is False
+    assert form.errors['company_numbers'] == [COMPANY_DOESNT_EXIST_MSG]
+
+    # some exist, some don't
+    company = CompanyFactory()
+    data = {
+        'company_numbers': '{num},23456789'.format(num=company.number)
+    }
+    form = CHNumbersForm(data=data)
+
+    assert form.is_valid() is False
+    assert form.errors['company_numbers'] == [COMPANY_DOESNT_EXIST_MSG]
+
+
+@pytest.mark.django_db
+def test_companies_publish_form_handles_whitespace():
+    companies = CompanyFactory.create_batch(3)
+    data = '	{num1},{num2} , {num3},'.format(
+        num1=companies[0].number, num2=companies[1].number,
+        num3=companies[2].number)
+    form = CHNumbersForm(data={'company_numbers': data})
+
+    assert form.is_valid() is True
