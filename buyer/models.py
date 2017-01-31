7a4772d90ac5103baf2c2f9a56adf93a052ec112
Monika Sulik <kontakt@monikasulik.net>
2017-01-31 10:24:33 +0000

    Set comment to blank=True to match validation on FAS form

--- buyer/models.py
+++ buyer/models.py
@@ -15,7 +15,7 @@ class Buyer(TimeStampedModel):
     )
     company_name = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
-    comment = models.TextField()
+    comment = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
