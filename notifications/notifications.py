d87fd9e3384fbf178040cb18f9ba2d78cbc0f168
Monika Sulik <kontakt@monikasulik.net>
2017-02-08 14:58:19 +0000

    Add TODO comments

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -1,14 +1,18 @@
 def no_case_studies():
+    # TODO: ED-918
     pass
 
 
 def hasnt_logged_in():
+    # TODO: ED-921
     pass
 
 
 def verification_code_not_given():
+    # TODO: ED-917
     pass
 
 
 def new_companies_in_sector():
+    # TODO: ED-919
     pass
