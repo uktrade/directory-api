ab120bacb940c6d31692928f3f15bfd62f227bf7
Monika Sulik <kontakt@monikasulik.net>
2017-02-08 18:03:46 +0000

    Change test name

--- notifications/tests/test_models.py
+++ notifications/tests/test_models.py
@@ -1,7 +1,7 @@
 from notifications.tests.factories import SupplierNotificationsFactory
 
 
-def test_suppliernotifications_unicode_method():
+def test_suppliernotifications_str_method():
     instance = SupplierNotificationsFactory.build(
         supplier__company_email='test@example.com',
         notification_type='no_case_studies'
