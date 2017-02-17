cd46931a6eacfd663cca8a93e733049d126231b2
konradko <konrad@korzel.com>
2017-02-17 17:32:21 +0000

    Fix notification tasks

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -4,24 +4,24 @@ from notifications import notifications
 
 
 @app.task
-def no_case_studies(self):
+def no_case_studies():
     with distributed_lock('no_case_studies'):
         notifications.no_case_studies()
 
 
 @app.task
-def hasnt_logged_in(self):
+def hasnt_logged_in():
     with distributed_lock('hasnt_logged_in'):
         notifications.hasnt_logged_in()
 
 
 @app.task
-def verification_code_not_given(self):
+def verification_code_not_given():
     with distributed_lock('verification_code_not_given'):
         notifications.verification_code_not_given()
 
 
 @app.task
-def new_companies_in_sector(self):
+def new_companies_in_sector():
     with distributed_lock('new_companies_in_sector'):
         notifications.new_companies_in_sector()
