d03524e027ea106bc8ec26331351e1fb86f83943
Rich Tier <rikatee@gmail.com>
2017-03-07 18:00:02 +0000

    Remove obsolete tests

--- company/tests/test_migrations.py
+++
@@ -1,9 +0,0 @@
-def example_test(migration):
-    app = 'company'
-    model_name = 'Company'
-    name = 'before_migration_name_here'
-    migration.before(app, name).get_model(app, model_name)
-
-    migration.apply('company', 'migration_name_here')
-
-    # assert migration did the job
