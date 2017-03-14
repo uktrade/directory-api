7ee3f35fb4bc0c104eeabfb13b70aea6c9b80f9f
Steven Burnell <steve.burnell@digital.trade.gov.uk>
2017-03-14 17:19:01 +0000

    Replace hardcoded image urls in emails (with input from Rich)

--- enrolment/constants.py
+++ enrolment/constants.py
@@ -2,3 +2,7 @@ EMAIL_HEADER_IMAGE = (
     'https://s3-eu-west-1.amazonaws.com/directory-email-static/'
     'exporting_is_great.png'
 )
+
+EMAIL_STATIC_FILE_BUCKET = (
+    'https://s3-eu-west-1.amazonaws.com/directory-email-static/'
+)
