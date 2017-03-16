e6d8197fbe885ef1779836701e6c6500ced602fb
konradko <konrad@korzel.com>
2017-03-16 12:06:28 +0000

    Improve style, add test env vars

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -78,20 +78,28 @@ def no_case_studies():
 
 def hasnt_logged_in():
     days_ago = datetime.utcnow() - timedelta(
-        days=settings.HASNT_LOGGED_IN_DAYS)
-    start_datetime = days_ago.replace(
-        hour=0, minute=0, second=0, microsecond=0)
-    end_datetime = days_ago.replace(
-        hour=23, minute=59, second=59, microsecond=999999)
-    login_data = sso_api_client.user.get_last_login(
-        start=start_datetime, end=end_datetime).json()
-    supplier_ids = [supplier['id'] for supplier in login_data]
-    suppliers = Supplier.objects.filter(
-        sso_id__in=supplier_ids
-    ).exclude(
-        supplieremailnotification__category=constants.HASNT_LOGGED_IN,
+        days=settings.HASNT_LOGGED_IN_DAYS
     )
+    start_datetime = days_ago.replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    end_datetime = days_ago.replace(
+        hour=23, minute=59, second=59, microsecond=999999
+    )
+
+    login_data = sso_api_client.user.get_last_login(
+        start=start_datetime, end=end_datetime
+    ).json()
+
     notification_category = constants.HASNT_LOGGED_IN
+
+    sso_ids = [sso_user['id'] for sso_user in login_data]
+    suppliers = Supplier.objects.filter(
+        sso_id__in=sso_ids
+    ).exclude(
+        supplieremailnotification__category=notification_category,
+    )
+
     for supplier in suppliers:
         send_email_notifications(
             recipient_name=supplier.name,
