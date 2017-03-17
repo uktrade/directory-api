0bbbb7be7a0468f2c49bc11ddd6343810999d0ad
Rich Tier <rikatee@gmail.com>
2017-03-17 19:07:02 +0000

    Hide unsubscribe url if cannot unsubscribe

--- api/tests/test_templates.py
+++ api/tests/test_templates.py
@@ -1,9 +1,27 @@
 from django.template.loader import render_to_string
 
 
+UNSUBSCRIBE_LABEL = 'Unsubscribe'
+
+
 def test_confirmation_email():
     context = {
         'url': 'http://confirm.com'
     }
     html = render_to_string('confirmation_email.html', context)
     assert 'http://confirm.com' in html
+
+
+def test_email_unsubscribe():
+    context = {
+        'unsubscribe_url': 'http://unsubscribe.com'
+    }
+    html = render_to_string('email.html', context)
+    assert context['unsubscribe_url'] in html
+    assert UNSUBSCRIBE_LABEL in html
+
+
+def test_email_unsubscribe_missing():
+    html = render_to_string('email.html', {})
+
+    assert UNSUBSCRIBE_LABEL not in html
