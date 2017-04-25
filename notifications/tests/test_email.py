55d0cdfb0a9d0c3ff09c84310c357c5f045247cb
Richard Tier <rihard@highertier.com>
2017-04-25 15:57:30 +0100

    Slice number of companies to 5 in new companies in sector

--- notifications/tests/test_email.py
+++ notifications/tests/test_email.py
@@ -16,3 +16,12 @@ def test_notification_base_rejects_if_required_properties_missing():
         "from_email, html_template, recipient, record_sent, subject, "
         "text_template, unsubscribe_url"
     )
+
+
+def test_new_companies_in_sector_limit_companies():
+    notification = email.NewCompaniesInSectorNotification(
+        subscriber={'email': 'jim@examle.com', 'name': 'Jim'},
+        companies=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    )
+    context = notification.get_context_data()
+    assert len(context['companies']) == 5
