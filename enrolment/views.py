f874faf55e73f6e2a0a012c09850718bf5802a76
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2017-05-31 13:58:08 +0100

    make process synchronous

--- enrolment/views.py
+++ enrolment/views.py
@@ -1,13 +1,18 @@
 import json
 
+from django.conf import settings
+from django.db import transaction
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
 import enrolment.queue
+from company.serializers import CompanySerializer
 
 from enrolment.models import Enrolment
 from enrolment import serializers
+from supplier.serializers import SupplierSerializer
 
 
 class EnrolmentCreateAPIView(CreateAPIView):
@@ -23,12 +28,55 @@ class EnrolmentCreateAPIView(CreateAPIView):
         })
         serializer.is_valid(raise_exception=True)
 
-        enrolment.queue.EnrolmentQueue().send(
-            data=json.dumps(request.data, ensure_ascii=False)
-        )
+        if settings.SQS_ENROLMENT_QUEUE_ENABLED:
+            enrolment.queue.EnrolmentQueue().send(
+                data=json.dumps(request.data, ensure_ascii=False)
+            )
+        else:
+            self.create_nested_objects(request.data)
 
         return Response(
             data=serializer.data,
             status=status.HTTP_202_ACCEPTED,
             headers=self.get_success_headers(serializer.data)
         )
+
+    @transaction.atomic
+    def create_nested_objects(self, data):
+        try:
+            company = self.create_company(
+                export_status=data['export_status'],
+                name=data['company_name'],
+                number=data['company_number'],
+                date_of_creation=data['date_of_creation'],
+            )
+            self.create_supplier(
+                company=company,
+                sso_id=data['sso_id'],
+                company_email=data['company_email'],
+            )
+        except KeyError as error:
+            raise ValidationError(
+                'Missing key: "{key}"'.format(key=error)
+            )
+
+    @staticmethod
+    def create_company(name, number, export_status, date_of_creation):
+        serializer = CompanySerializer(data={
+            'name': name,
+            'number': number,
+            'export_status': export_status,
+            'date_of_creation': date_of_creation,
+        })
+        serializer.is_valid(raise_exception=True)
+        return serializer.save()
+
+    @staticmethod
+    def create_supplier(sso_id, company_email, company):
+        serializer = SupplierSerializer(data={
+            'sso_id': sso_id,
+            'company_email': company_email,
+            'company': company.pk,
+        })
+        serializer.is_valid(raise_exception=True)
+        return serializer.save()
