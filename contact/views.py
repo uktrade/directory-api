ad2b3e183b28a5e6a9ccc0c62d2a8c6d73a39ad5
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2017-06-12 12:16:09 +0100

    make job async

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,16 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from contact.serializers import MessageToSupplierSerializer
+from .serializers import MessageToSupplierSerializer
+from .tasks import message_to_supplier
 
 
-class CreateMessageToSupplierAPIView(CreateAPIView):
+class CreateMessageToSupplierAPIView(APIView):
     serializer_class = MessageToSupplierSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message_to_supplier.delay(data=serializer.validated_data)
+        return Response(status=status.HTTP_202_ACCEPTED)
