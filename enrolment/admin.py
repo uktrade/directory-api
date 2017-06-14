8c88400e6b137c8cbf22274502c74fff1fa19e17
Richard Tier <rihard@highertier.com>
2017-06-14 15:50:01 +0100

    Remove Enrolment model

--- enrolment/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from enrolment.models import Enrolment
-
-
-@admin.register(Enrolment)
-class EnrolmentAdmin(admin.ModelAdmin):
-    search_fields = ('data', )
-    readonly_fields = ('created', 'modified',)
