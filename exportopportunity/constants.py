7d4ffd41a02695baa379f1b7bdb72133ec284d10
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2017-07-07 16:39:00 +0100

    Add model

---
+++ exportopportunity/constants.py
@@ -0,0 +1,24 @@
+SPECIFIC = 'specific'
+OPEN_ENDED = 'open_ended'
+DISTRIBUTION = 'distribution'
+WHOLESALE = 'wholesale'
+RETAIL = 'retail'
+HOSPITALITY = 'hospitality'
+CATERING = 'catering'
+MANUFACTURING = 'manufacturing'
+
+TYPE_OF_ENQUIRIES = (
+    SPECIFIC, 'Specific',
+    OPEN_ENDED, 'Open-ended'
+)
+BUSINESS_MODELS = (
+    DISTRIBUTION, 'Distribution',
+    WHOLESALE, 'Wholesale',
+    RETAIL, 'Retail'
+)
+SUBSECTOR_SELECTION = (
+    RETAIL, 'Retail',
+    HOSPITALITY, 'Hospitality',
+    CATERING, 'Catering',
+    MANUFACTURING, 'Manufacturing'
+)
--- exportopportunity/models.py
+++ exportopportunity/models.py
@@ -1,5 +1,26 @@
+from django.db import models
+
 from api.model_utils import TimeStampedModel
+
+from . import constants as choices
 
 
 class ExportOpportunity(TimeStampedModel):
-    pass
+    type_of_enquiry = models.CharField(
+        max_length=10,
+        choices=choices.TYPE_OF_ENQUIRIES
+    )
+    open_ended_description = models.TextField(blank=True, default='')
+    business_model = models.CharField(
+        max_length=20,
+        choices=choices.BUSINESS_MODELS
+    )
+    subsector = models.CharField(
+        max_length=20,
+        choices=choices.SUBSECTOR_SELECTION
+    )
+    bid_value = models.CharField(max_length=50)
+    bid_timing = models.DateField()
+    full_name = models.CharField(max_length=255)
+    email_address = models.EmailField()
+    company_name = models.CharField(max_length=255)
