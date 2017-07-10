6ded0069e0e171676ea54ac1fc4d42a82b826e49
konradko <konrad@korzel.com>
2017-07-10 14:54:59 +0100

    Remove swagger

--- api/views.py
+++ api/views.py
@@ -1,17 +1,8 @@
 import json
 
-from rest_framework import response, schemas, status
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
-
-
-@api_view()
-@renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
-def documentation(request):
-    generator = schemas.SchemaGenerator(title='Export Directory API')
-    return response.Response(generator.get_schema(request=request))
 
 
 class HealthCheckAPIView(APIView):
