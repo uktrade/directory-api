e3cc37ed481ef6be015b6228009dd01dbea4cce0
Alessandro De Noia <alessandro@sdonklabs.com>
2017-09-06 10:13:22 +0100

    Added user is company owner model

--- user/models.py
+++ user/models.py
@@ -52,6 +52,8 @@ class User(TimeStampedModel):
             'Designates whether this user should receive notifications'
         ),
     )
+    is_company_owner = models.BooleanField(default=False)
+
 
     class Meta:
         ordering = ('-created', '-modified')
