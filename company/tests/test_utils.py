0ebd51497fa4bfa818c17bb7f51aeffc575fbbcf
richtier <rikatee@gmail.com>
2017-09-22 15:32:23 +0100

    Populate case study elasticsearch index

--- company/tests/test_utils.py
+++ company/tests/test_utils.py
@@ -5,15 +5,15 @@ import pytest
 from django.utils import timezone
 from freezegun import freeze_time
 
-from company.tests.factories import CompanyFactory
-from company import models, utils
+from company.tests import factories
+from company import models, search, utils
 
 
 @pytest.mark.django_db
 @freeze_time()
 @mock.patch('company.utils.stannp_client')
 def test_send_letter(mock_stannp_client):
-    company = CompanyFactory(verification_code='test')
+    company = factories.CompanyFactory(verification_code='test')
     utils.send_verification_letter(company)
     mock_stannp_client.send_letter.assert_called_with(
         recipient={
@@ -40,12 +40,28 @@ def test_send_letter(mock_stannp_client):
 
 
 @pytest.mark.django_db
-@mock.patch.object(utils, 'Index')
-def test_rebuild_and_populate_elasticsearch_index(mock_index):
-    CompanyFactory.create()
+@pytest.mark.rebuild_elasticsearch
+def test_rebuild_and_populate_elasticsearch_index():
+    published_company = factories.CompanyFactory(is_published=True)
+    unpublished_company = factories.CompanyFactory(is_published=False)
 
+    published_case_study = factories.CompanyCaseStudyFactory(
+        company=published_company
+    )
+    unpublished_case_study = factories.CompanyCaseStudyFactory(
+        company=unpublished_company
+    )
+
+    search.CompanyDocType.get(id=published_company.pk).delete()
+    search.CaseStudyDocType.get(id=published_case_study.pk).delete()
     utils.rebuild_and_populate_elasticsearch_index(models.Company)
 
-    mock_index.assert_called_with('companies')
-    assert mock_index().delete.call_count == 1
-    assert mock_index().create.call_count == 1
+    assert search.CompanyDocType.get(id=published_company.pk) is not None
+    assert search.CaseStudyDocType.get(id=published_case_study.pk) is not None
+
+    assert search.CompanyDocType.get(
+        id=unpublished_company.pk, ignore=404
+    ) is None
+    assert search.CaseStudyDocType.get(
+        id=unpublished_case_study.pk, ignore=404
+    ) is None
