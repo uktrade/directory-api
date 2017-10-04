c91d8d348a7a6a5faaf9a6536a0b04c381d7db7c
richtier <rikatee@gmail.com>
2017-10-04 19:37:33 +0100

    Fix doctypes in migrations

--- company/search.py
+++ company/search.py
@@ -92,68 +92,81 @@ def get_absolute_url(url):
 
 
 def company_model_to_doc_type(company):
+    # getattr is used on the company to allow this functionton be used in
+    # migrations (historic models wont have all the fields listed below).
+    company_fields = {
+        'date_of_creation',
+        'description',
+        'employees',
+        'facebook_url',
+        'keywords',
+        'linkedin_url',
+        'modified',
+        'name',
+        'number',
+        'sectors',
+        'slug',
+        'summary',
+        'twitter_url',
+        'website',
+        'campaign_tag',
+    }
+    case_study_fields = {
+        'description',
+        'image_one_caption',
+        'image_three_caption',
+        'image_two_caption',
+        'keywords',
+        'pk',
+        'sector',
+        'short_summary',
+        'slug',
+        'testimonial',
+        'testimonial_company',
+        'testimonial_job_title',
+        'testimonial_name',
+        'title',
+        'website',
+    }
+    has_description = getattr(company, 'description', '') != ''
     company_doc_type = CompanyDocType(
         meta={'id': company.pk},
-        case_study_count=company.supplier_case_studies.count(),
-        date_of_creation=company.date_of_creation,
-        description=company.description,
-        has_description=company.description != '',
-        employees=company.employees,
-        facebook_url=company.facebook_url,
-        has_single_sector=len(company.sectors) == 1,
-        keywords=company.keywords,
-        linkedin_url=company.linkedin_url,
-        logo=get_absolute_url(company.logo.url if company.logo else ''),
-        modified=company.modified,
-        name=company.name,
-        number=company.number,
         pk=str(company.pk),
-        sectors=company.sectors,
+        case_study_count=company.supplier_case_studies.count(),
+        has_single_sector=len(company.sectors) == 1,
+        has_description=has_description,
+        logo=get_absolute_url(company.logo.url if company.logo else ''),
         sectors_label=[helpers.get_sector_label(v) for v in company.sectors],
-        slug=company.slug,
-        summary=company.summary,
-        twitter_url=company.twitter_url,
-        website=company.website,
-        campaign_tag=company.campaign_tag,
+        **{key: getattr(company, key, '') for key in company_fields},
     )
     for case_study in company.supplier_case_studies.all():
         company_doc_type.supplier_case_studies.append({
-            'description': case_study.description,
-            'image_one_caption': case_study.image_one_caption,
-            'image_three_caption': case_study.image_three_caption,
-            'image_two_caption': case_study.image_two_caption,
-            'keywords': case_study.keywords,
-            'pk': case_study.pk,
-            'sector': case_study.sector,
-            'short_summary': case_study.short_summary,
-            'slug': case_study.slug,
-            'testimonial': case_study.testimonial,
-            'testimonial_company': case_study.testimonial_company,
-            'testimonial_job_title': case_study.testimonial_job_title,
-            'testimonial_name': case_study.testimonial_name,
-            'title': case_study.title,
-            'website': case_study.website,
+            key: getattr(case_study, key, '') for key in case_study_fields
         })
     return company_doc_type
 
 
 def case_study_model_to_doc_type(case_study):
-    case_study_doc_type = CaseStudyDocType(
+    # getattr is used on the case study to allow this functionton be used in
+    # migrations (historic models wont have all the fields listed below).
+    fields = {
+        'description',
+        'image_one_caption',
+        'image_three_caption',
+        'image_two_caption',
+        'keywords',
+        'pk',
+        'sector',
+        'short_summary',
+        'slug',
+        'title',
+        'campaign_tag',
+    }
+    return CaseStudyDocType(
         meta={'id': case_study.pk},
-        description=case_study.description,
-        image_one_caption=case_study.image_one_caption,
-        image_three_caption=case_study.image_three_caption,
-        image_two_caption=case_study.image_two_caption,
-        keywords=case_study.keywords,
-        pk=case_study.pk,
-        sector=case_study.sector,
-        short_summary=case_study.short_summary,
-        slug=case_study.slug,
-        title=case_study.title,
-        company_number=case_study.company.number,
         image=get_absolute_url(
             case_study.image_one.url if case_study.image_one else '',
         ),
-        campaign_tag=case_study.campaign_tag,
+        company_number=case_study.company.number,
+        **{key: getattr(case_study, key, '') for key in fields},
     )
-    return case_study_doc_type
