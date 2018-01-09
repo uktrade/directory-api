863d1dcf0314e9297877fda6df9948e3a4d3c0f9
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-01-09 16:28:49 +0000

    fix pep8 issue

--- exportopportunity/tests/test_admin.py
+++ exportopportunity/tests/test_admin.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
 
+
 class ExportOpportunityAdminTestCase(TestCase):
 
     def setUp(self):
