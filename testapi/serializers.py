a5c407152f74627d353e3d2d5d00483ecd31fd37
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-30 16:45:26 +0000

    removed unwanted field [skip ci]

--- testapi/serializers.py
+++ testapi/serializers.py
@@ -8,5 +8,4 @@ from rest_framework.serializers import (
 class UserSerializer(Serializer):
 
     sso_id = CharField(max_length=200, read_only=True)
-    email_verification_link = CharField(max_length=500, read_only=True)
     is_verified = BooleanField(read_only=True)
