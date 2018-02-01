b5ffe914e85434805c7b0fc83308cf8475d1ba77
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-02-01 16:06:58 +0000

    Filter out suppliers without company

--- supplier/tests/test_management_commands.py
+++ supplier/tests/test_management_commands.py
@@ -9,10 +9,18 @@ from supplier.tests.factories import SupplierFactory
 
 @pytest.mark.django_db
 @mock.patch(
-    'supplier.management.commands.generate_suppliers_csv_dump.upload_file_object_to_s3'  # NOQA
+    'supplier.management.commands.generate_suppliers_csv_dump.upload_file_object_to_s3'
+    # NOQA
 )
 def test_upload_suppliers_csv_to_s3(mocked_upload_file_object_to_s3):
     SupplierFactory.create_batch(5)
+    SupplierFactory(
+        sso_id=123,
+        name='foobar',
+        company_email='foobar%s@example.com',
+        company=None,
+        is_company_owner=True,
+    )
     call_command('generate_suppliers_csv_dump')
     assert mocked_upload_file_object_to_s3.called
     assert mocked_upload_file_object_to_s3.call_args == mock.call(
