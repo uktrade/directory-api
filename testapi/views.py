4ceb78892471a93c968fccce5751272c6e1ad7b1
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-12 22:50:13 +0000

    remove type hinting

--- testapi/views.py
+++ testapi/views.py
@@ -4,7 +4,6 @@ from rest_framework.generics import (
     DestroyAPIView,
     RetrieveAPIView
 )
-from rest_framework.request import Request
 from rest_framework.response import Response
 
 from django.conf import settings
@@ -25,7 +24,7 @@ class CompanyTestAPIView(RetrieveAPIView, DestroyAPIView):
             raise Http404
         return super().dispatch(*args, **kwargs)
 
-    def get(self, request: Request, ch_id: str):
+    def get(self, request, ch_id):
         company = get_object_or_404(Company, number=ch_id)
         response_data = {
             'letter_verification_code': company.verification_code,
@@ -34,7 +33,7 @@ class CompanyTestAPIView(RetrieveAPIView, DestroyAPIView):
         }
         return Response(response_data)
 
-    def delete(self, request: Request, ch_id: str):
+    def delete(self, request, ch_id):
         try:
             company = get_object_or_404(Company, number=ch_id)
             company.delete()
