42df5ab55c4263be61e05e3a267fe6494740b360
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-14 10:08:01 +0000

    add view for getting list of published companies

--- testapi/views.py
+++ testapi/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from django.conf import settings
 from api.signature import SignatureCheckPermission
 from company.models import Company
-from testapi.serializers import CompanySerializer
+from testapi.serializers import CompanySerializer, PublishedCompaniesSerializer
 
 
 class CompanyTestAPIView(RetrieveAPIView, DestroyAPIView):
@@ -41,3 +41,45 @@ class CompanyTestAPIView(RetrieveAPIView, DestroyAPIView):
         ch_id = kwargs['ch_id']
         self.get_company(ch_id).delete()
         return Response(status=204)
+
+
+class PublishedCompaniesTestAPIView(RetrieveAPIView):
+    serializer_class = PublishedCompaniesSerializer
+    queryset = Company.objects.filter(is_published=True)
+    permission_classes = [SignatureCheckPermission]
+    lookup_field = 'is_published'
+    http_method_names = 'get'
+
+    def dispatch(self, *args, **kwargs):
+        if not settings.FEATURE_TEST_API_ENABLED:
+            raise Http404
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        params = request.query_params
+        limit = int(params.get('limit', 100))
+        minimal_number_of_sectors = int(params.get(
+            'minimal_number_of_sectors', 0))
+        response_data = []
+        counter = 0
+
+        for company in self.queryset.all():
+            if len(company.sectors) >= minimal_number_of_sectors:
+                counter += 1
+                if counter <= limit:
+                    data = {
+                        'name': company.name,
+                        'number': company.number,
+                        'sectors': company.sectors,
+                        'employees': company.employees,
+                        'keywords': company.keywords,
+                        'website': company.website,
+                        'facebook_url': company.facebook_url,
+                        'twitter_url': company.twitter_url,
+                        'linkedin_url': company.linkedin_url,
+                        'company_email': company.email_address,
+                        'summary': company.summary,
+                        'description': company.description,
+                    }
+                    response_data.append(data)
+        return Response(response_data)
