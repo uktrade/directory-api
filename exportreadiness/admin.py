2de691f7b4d975fcd3c19c38684075f259d393d9
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-03-08 08:11:50 +0000

    Triage results download

--- exportreadiness/admin.py
+++ exportreadiness/admin.py
@@ -1,5 +1,7 @@
+import datetime
 from django.contrib import admin
 
+from api.utils import generate_csv_response
 from exportreadiness.models import TriageResult
 
 
@@ -22,4 +24,20 @@ class TriageResultAdmin(admin.ModelAdmin):
         'used_online_marketplace',
         'sector',
         'is_in_companies_house',
+    )
+    actions = ['download_csv']
+    csv_excluded_fields = []
+
+    csv_filename = 'export-readiness_triage_{}.csv'.format(
+        datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+
+    def download_csv(self, request, queryset):
+        return generate_csv_response(
+            queryset=queryset,
+            filename=self.csv_filename,
+            excluded_fields=self.csv_excluded_fields
+        )
+
+    download_csv.short_description = (
+        "Download CSV report for selected triage results"
     )
