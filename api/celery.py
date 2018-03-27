6dcfc6979bf14143ed44707fcd31e9d0be932006
konradko <konrad@korzel.com>
2018-03-27 22:19:52 +0100

    Add setting celery ssl with a flag, update redis and kombu

--- api/celery.py
+++ api/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import logging
 import os
+from ssl import CERT_NONE
 
 from django.conf import settings
 
@@ -19,6 +20,16 @@ app = Celery('api')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+if settings.FEATURE_REDIS_USE_SSL:
+    ssl_conf = {
+        'ssl_cert_reqs': CERT_NONE,
+        'ssl_ca_certs': None,
+        'ssl_certfile': None,
+        'ssl_keyfile': None
+    }
+    app.conf.broker_use_ssl = ssl_conf
+    app.conf.redis_backend_use_ssl = ssl_conf
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
