005f1909cea355fdaa1e43e5bffbc6fca1c77cdd
konradko <konrad@korzel.com>
2018-06-07 17:17:36 +0100

    Change HEROKU_API_KEY to HEROKU_TOKEN

--- docker/install_heroku_cli.sh
+++ docker/install_heroku_cli.sh
@@ -9,9 +9,9 @@ heroku -v
 cat >~/.netrc <<EOF
 machine api.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_KEY
+  password $HEROKU_TOKEN
 machine git.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_KEY
+  password $HEROKU_TOKEN
 EOF
 chmod 600 ~/.netrc # Heroku cli complains about permissions without this
