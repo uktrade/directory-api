e287b5ec09717d3a67d79fab0eac7e30bdc78c76
richtier <rikatee@gmail.com>
2018-07-24 18:03:18 +0100

    Reject request to admin if cannot determine IP address

--- core/middleware.py
+++ core/middleware.py
@@ -1,8 +1,24 @@
+import logging
+
 from admin_ip_restrictor import middleware
+
+from django.http import Http404
 
 from core import helpers
 
 
+logger = logging.getLogger(__name__)
+
+
 class AdminIPRestrictorMiddleware(middleware.AdminIPRestrictorMiddleware):
+    MESSAGE_UNABLE_TO_DETERMINE_IP_ADDRESS = 'Unable to determine remote IP'
+
     def get_ip(self, request):
         return helpers.RemoteIPAddressRetriver().get_ip_address(request)
+
+    def process_request(self, request):
+        try:
+            return super().process_request(request)
+        except LookupError:
+            logger.exception(self.MESSAGE_UNABLE_TO_DETERMINE_IP_ADDRESS)
+            raise Http404()
--- core/tests/test_middleware.py
+++ core/tests/test_middleware.py
@@ -1,4 +1,9 @@
 from unittest import mock
+
+import pytest
+
+from django.http import Http404
+
 from core import constants, middleware
 
 
@@ -10,3 +15,22 @@ def test_admin_ip_restrictor_middleware(mock_get_ip_address, settings, rf):
 
     assert mock_get_ip_address.call_count == 1
     assert mock_get_ip_address.call_args == mock.call(request)
+
+
+@mock.patch('core.helpers.IpwareRemoteIPAddressRetriver.get_ip_address')
+def test_admin_ip_restrictor_middleware_handles_unable_to_determine_ip(
+    mock_get_ip_address, settings, caplog, rf
+):
+    settings.REMOTE_IP_ADDRESS_RETRIEVER = constants.IP_RETRIEVER_NAME_IPWARE
+    settings.RESTRICT_ADMIN = True
+
+    mock_get_ip_address.side_effect = LookupError()
+
+    request = rf.get('/')
+    instance = middleware.AdminIPRestrictorMiddleware()
+    with pytest.raises(Http404):
+        instance.process_request(request)
+
+    log = caplog.records[0]
+    assert log.levelname == 'ERROR'
+    assert log.msg == instance.MESSAGE_UNABLE_TO_DETERMINE_IP_ADDRESS
