be1bd84825fcc0c0d83f78256fe4e6f4d4fecba4
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-04-23 15:52:05 +0100

    added v1 isd basic term search

--- company/tests/test_views.py
+++ company/tests/test_views.py
@@ -1118,19 +1118,27 @@ def test_company_search(mock_get_search_results, api_client):
     )
 
 
-def test_investment_support_directory(api_client):
+@patch('elasticsearch_dsl.response.Response.to_dict')
+def test_investment_support_directory(mock_get_search_results, api_client):
+
+    mock_get_search_results.return_value = expected_value = {
+        'hits': {
+            'total': 2,
+            'hits': [None, None],
+        },
+    }
 
     data = {
         'term': 'bones',
         'page': 1,
         'size': 10,
-        'expertise_industries': [choices.INDUSTRIES[0][0]],
+        'sectors': [choices.INDUSTRIES[0][0]],
     }
     response = api_client.get(reverse(
         'investment-support-directory-search'), data=data)
 
     assert response.status_code == 200
-    assert response.json() == {'Results': 'ISD_Search_PlaceHolder'}
+    assert response.json() == expected_value
 
 
 @patch('company.views.CompanySearchAPIView.get_search_results')
