86ec01f4a150474ff29b9e81cd0c3a2ff0c9bc1d
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-05-03 10:48:59 +0100

    fix search

--- company/search.py
+++ company/search.py
@@ -45,7 +45,7 @@ class CompanyDocType(DocType):
     expertise_languages = field.Text(multi=True)
     expertise_countries = field.Text(multi=True)
     # This resolves as a dict, allowing dict with list values.
-    expertise_products_service = field.Text(multi=True)
+    expertise_products_services = field.Text(multi=True)
     expertise_products_services_labels = field.Text(multi=True)
     expertise_labels = field.Text(multi=True)
     slug = field.Text()
