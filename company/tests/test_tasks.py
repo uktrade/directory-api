58e1171d0b6b4bd070e0fd6856efb333b0f5341a
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-07-29 15:38:11 +0100

    renamed tests

--- company/tests/test_tasks.py
+++ company/tests/test_tasks.py
@@ -5,7 +5,7 @@ from company.tasks import retrieve_companies_house_company_status
 
 @mock.patch('company.tasks.lock_acquired', mock.Mock(return_value=True))
 @mock.patch('company.tasks.call_command')
-def retrieve_companies_house_company_status(mocked_call_command):
+def test_retrieve_companies_house_company_status(mocked_call_command):
     retrieve_companies_house_company_status()
     mocked_call_command.assert_called_once_with(
         'retrieve_companies_house_company_status'
