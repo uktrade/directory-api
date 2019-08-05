565b3fe55c38e7d698b79725be216f5e344e13b9
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-08-05 19:03:35 +0100

    tidy syntax

--- core/middleware.py
+++ core/middleware.py
@@ -21,8 +21,7 @@ class SignatureCheckMiddleware(
 class AdminPermissionCheckMiddleware(MiddlewareMixin):
 
     def process_view(self, request, view_func, view_args, view_kwarg):
-        if request.user is not None and (
-                request.resolver_match.namespace == 'admin' or request.path_info.startswith('/admin/login')
-        ):
-            if not request.user.is_staff and request.user.is_authenticated():
-                return HttpResponse('User not authorized for admin access', status=401)
+        if request.user is not None:
+            if request.resolver_match.namespace == 'admin' or request.path_info.startswith('/admin/login'):
+                if not request.user.is_staff and request.user.is_authenticated():
+                    return HttpResponse('User not authorized for admin access', status=401)
