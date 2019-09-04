e2e9f7255c3e64869354720d4f24d677efd9f3f1
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-09-04 13:18:55 +0100

    fix reload conf

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -6,19 +6,16 @@ from django.urls import clear_url_caches
 from django.urls import reverse
 
 
-def reload_modules_urlconf(modules=None, urlconf=None):
-    def reload_module(module_name):
-        if module_name in sys.modules:
-            reload(sys.modules[module_name])
-        else:
-            import_module(module_name)
-    if modules:
-        for module in modules:
-            reload_module(module)
+def reload_urlconf():
     clear_url_caches()
-    if urlconf is None:
-        urlconf = settings.ROOT_URLCONF
-    reload_module(urlconf)
+    reload_module(settings.ROOT_URLCONF)
+
+
+def reload_module(module):
+    if module in sys.modules:
+        reload(sys.modules[module])
+    else:
+        import_module(module)
 
 
 def test_force_staff_sso(client):
@@ -27,7 +24,7 @@ def test_force_staff_sso(client):
     settings.AUTHBROKER_CLIENT_ID = 'debug'
     settings.AUTHBROKER_CLIENT_SECRET = 'debug'
     settings.AUTHBROKER_URL = 'https://test.com'
-    reload_modules_urlconf()
+    reload_urlconf()
 
     assert reverse('authbroker_client:login') == '/auth/login/'
     assert reverse('authbroker_client:callback') == '/auth/callback/'
@@ -36,4 +33,4 @@ def test_force_staff_sso(client):
     assert response.url == '/auth/login/'
 
     settings.FEATURE_ENFORCE_STAFF_SSO_ENABLED = False
-    reload_modules_urlconf()
+    reload_urlconf()
