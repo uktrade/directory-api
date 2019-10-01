c50e179463e8b4f0afe6a13031efc65f88bbb557
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-01 18:42:49 +0100

    testapi: return additional company details

--- testapi/serializers.py
+++ testapi/serializers.py
@@ -18,6 +18,8 @@ class CompanySerializer(Serializer):
     letter_verification_code = CharField(max_length=10, read_only=True)
     company_email = CharField(max_length=200, read_only=True)
     is_verification_letter_sent = BooleanField(read_only=True)
+    is_identity_check_message_sent = BooleanField(read_only=True)
+    verified_with_identity_check = BooleanField(read_only=True)
 
 
 class ISDCompanySerializer(ModelSerializer):
