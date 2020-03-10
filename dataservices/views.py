66b464a09d3209c28c54d342f55db79f9e4ee113
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-03-10 18:21:40 +0000

    added views

--- dataservices/views.py
+++ dataservices/views.py
@@ -1,11 +1,12 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework import status, generics
 
-from dataservices import serializers, models
+from dataservices import serializers, models, helpers
 from rest_framework.response import Response
 from django.http import Http404
+from requests.exceptions import HTTPError
 
 
-class RetrieveEaseOfBusinessIndex(RetrieveAPIView):
+class RetrieveEaseOfBusinessIndex(generics.RetrieveAPIView):
     serializer_class = serializers.EaseOfDoingBusinessSerializer
     permission_classes = []
     lookup_url_kwarg = 'country_code'
@@ -18,7 +19,7 @@ class RetrieveEaseOfBusinessIndex(RetrieveAPIView):
         return super().handle_exception(exc)
 
 
-class RetrieveCorruptionPerceptionsIndex(RetrieveAPIView):
+class RetrieveCorruptionPerceptionsIndex(generics.RetrieveAPIView):
     serializer_class = serializers.CorruptionPerceptionsIndexSerializer
     permission_classes = []
     lookup_url_kwarg = 'country_code'
@@ -29,3 +30,49 @@ class RetrieveCorruptionPerceptionsIndex(RetrieveAPIView):
         if isinstance(exc, Http404):
             return Response(data={})
         return super().handle_exception(exc)
+
+
+class RetrieveLastYearImportDataView(generics.GenericAPIView):
+    permission_classes = []
+
+    def get(self, *args, **kwargs):
+        commodity_code = self.request.GET.get('commodity_code', '')
+        country = self.request.GET.get('country', '')
+
+        try:
+            comtrade = helpers.ComTradeData(commodity_code=commodity_code, reporting_area=country)
+            last_year_data = comtrade.get_last_year_import_data()
+            return Response(
+                status=status.HTTP_200_OK,
+                data={'last_year_data': last_year_data}
+            )
+        except HTTPError:
+            return Response(
+                status=500,
+                data={'error_message': 'Connection to Comtrade failed'}
+            )
+
+
+class RetrieveHistoricalImportDataView(generics.GenericAPIView):
+    permission_classes = []
+
+    def get(self, *args, **kwargs):
+        commodity_code = self.request.GET.get('commodity_code', '')
+        country = self.request.GET.get('country', '')
+
+        try:
+            comtrade = helpers.ComTradeData(commodity_code=commodity_code, reporting_area=country)
+
+            historical_data = {'historical_import_data': []}
+            historical_data['historical_import_data'].append(comtrade.get_historical_import_value_partner_country())
+            historical_data[
+                'historical_import_data'].append(comtrade.get_historical_import_value_world())
+            return Response(
+                status=status.HTTP_200_OK,
+                data=historical_data
+            )
+        except HTTPError:
+            return Response(
+                status=500,
+                data={'error_message': 'Connection to Comtrade failed'}
+            )
