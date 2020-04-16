50d65f9950a0136e5e6a449a7ac7d55256d5a769
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-04-16 14:31:04 +0100

    simplify

--- exportplan/signals.py
+++ exportplan/signals.py
@@ -32,8 +32,6 @@ def add_target_markets_data(sender, instance, *args, **kwargs):
         })
 
         if settings.FEATURE_COMTRADE_HISTORICAL_DATA_ENABLED:
-            target_market.update({
-                'historical_import_data': helpers.get_historical_import_data(
-                    commodity_code=commodity_code, country=country
-                ),
-            })
+            target_market['historical_import_data'] = helpers.get_historical_import_data(
+                commodity_code=commodity_code, country=country
+            )
