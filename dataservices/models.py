e26d1aab95bc78fad91fbf0ea3c8819970378fc4
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2020-11-25 11:32:55 +0000

    Added GDP Per Capita data into directory-api

--- dataservices/models.py
+++ dataservices/models.py
@@ -88,3 +88,12 @@ class Country(TimeStampedModel):
     class Meta:
         verbose_name = 'Country'
         verbose_name_plural = 'Countries'
+
+
+class GDPPerCapita(TimeStampedModel):
+    country_name = models.CharField(unique=False, blank=False, null=False, max_length=255)
+    country_code = models.CharField(unique=False, blank=False, null=False, max_length=50)
+    year_2019 = models.DecimalField(null=True, blank=True, decimal_places=3, max_digits=15)
+
+    def __str__(self):
+        return self.country_name
