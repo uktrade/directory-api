6d5b47d007657be99c9f0af88e3c0bded782d0f4
Bob Meredith <bob.meredith@ntlworld.com>
2021-01-22 13:54:56 +0000

    Switch to totals in serializers and added hardcoded year

--- dataservices/serializers.py
+++ dataservices/serializers.py
@@ -6,6 +6,7 @@ from dataservices import models
 class EaseOfDoingBusinessSerializer(serializers.ModelSerializer):
     total = serializers.SerializerMethodField()
     country = serializers.SerializerMethodField()
+    year = serializers.SerializerMethodField()
 
     class Meta:
         model = models.EaseOfDoingBusiness
@@ -18,17 +19,30 @@ class EaseOfDoingBusinessSerializer(serializers.ModelSerializer):
         if obj.country:
             return obj.country.name
 
+    def get_year(self, obj):
+        # The year is implicit and should be updated when new data are imported
+        return '2019'
+
 
 class CorruptionPerceptionsIndexSerializer(serializers.ModelSerializer):
     country = serializers.SerializerMethodField()
+    total = serializers.SerializerMethodField()
+    year = serializers.SerializerMethodField()
 
     class Meta:
         model = models.CorruptionPerceptionsIndex
         exclude = ['created', 'id', 'modified']
 
+    def get_total(self, obj):
+        return models.CorruptionPerceptionsIndex.objects.all().count()
+
     def get_country(self, obj):
         if obj.country:
             return obj.country.name
+
+    def get_year(self, obj):
+        # The year is implicit and should be updated when new data are imported
+        return '2019'
 
 
 class WorldEconomicOutlookSerializer(serializers.ModelSerializer):
