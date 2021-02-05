f76f8453d8bf634c3beb7e4e02ad4014bd7d3b12
Rafa Garcia <rafa@rafagarcia.info>
2021-02-05 14:39:10 +0000

    Change managements command to fetch S3 resources

--- company/management/commands/import_hscodes.py
+++ company/management/commands/import_hscodes.py
@@ -1,25 +1,28 @@
 import pathlib
-
 import tablib
+from django.conf import settings
 from django.core.management import BaseCommand
 
 from company.admin import HsCodeSectorResource
 from company.models import HsCodeSector
+from core.helpers import get_file_from_s3
 
 
 class Command(BaseCommand):
-
     help = 'Command utility to load HS code to DB.'
 
     def handle(self, *args, **options):
-        dir_path = pathlib.Path(__file__).parent.parent
-        with open(str(dir_path) + '/files/HS4.sectormap.v1.csv', 'r', encoding='utf-8-sig') as f:
-            dataset = tablib.import_set(f.read(), format='csv', headers=True)
-            resource = HsCodeSectorResource()
-            result = resource.import_data(dataset, dry_run=True)
-            self.stdout.write(self.style.SUCCESS(result.has_errors()))
-            if not result.has_errors():
-                HsCodeSector.objects.all().delete()
-                resource.import_data(dataset, dry_run=False)
+        file_name = 'HS4.sectormap.v1.csv'
+        bucket = settings.AWS_STORAGE_BUCKET_NAME_DATA_SCIENCE
+        s3_resource = get_file_from_s3(bucket, file_name)
+        csv_file = s3_resource['Body'].read().decode('utf-8')
+        dataset = tablib.import_set(csv_file, format='csv', headers=True)
+        resource = HsCodeSectorResource()
+        result = resource.import_data(dataset, dry_run=True)
+
+        self.stdout.write(self.style.SUCCESS(result.has_errors()))
+        if not result.has_errors():
+            HsCodeSector.objects.all().delete()
+            resource.import_data(dataset, dry_run=False)
 
         self.stdout.write(self.style.SUCCESS('All done, bye!'))
