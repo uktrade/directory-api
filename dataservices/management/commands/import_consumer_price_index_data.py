ffeb4c335aceb3e35d5f8fe7abb6cd7cae7729e1
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-03-03 11:04:45 +0000

    Refactor CPI management commad

--- dataservices/management/commands/import_consumer_price_index_data.py
+++ dataservices/management/commands/import_consumer_price_index_data.py
@@ -1,4 +1,6 @@
-import tablib
+import csv
+import re
+
 from django.core.management import BaseCommand
 
 from dataservices.models import ConsumerPriceIndex, Country
@@ -14,22 +16,28 @@ class Command(BaseCommand):
         # https://data.worldbank.org/indicator/IT.NET.USER.ZS
         # TODO refactor merge structures and more intellect import without file manipulation
         with open('dataservices/resources/Consumer_Price_Index.csv', 'r', encoding='utf-8-sig') as f:
-            data = tablib.import_set(f.read(), format='csv', headers=True)
+            file_reader = csv.DictReader(f)
             cpi_data = []
-
-            for item in data:
+            for row in file_reader:
+                store = {}
+                for col_name, value in row.items():
+                    # Gather data for several years
+                    match = re.match('^\\d{4}$', col_name)
+                    if match and value:
+                        year = match.group(0)
+                        store['year'] = year
+                        store['value'] = value
+                # delete existing record so it can be updated with latest CSV data
+                # this is due to if we have updated csv in later on the year for this data
+                if row.get('ISO'):
+                    existing_record = ConsumerPriceIndex.objects.filter(country_code=row.get('ISO'), year=year)
+                    existing_record.delete()
                 try:
-                    country = Country.objects.get(iso3=item[1])
+                    country = Country.objects.get(iso3=row.get('ISO'))
                 except Country.DoesNotExist:
                     country = None
-                cpi_data.append(
-                    ConsumerPriceIndex(
-                        country_name=item[0],
-                        country_code=item[1],
-                        country=country,
-                        year=2020,  # Need to amend based on year in CSV
-                        value=item[4] if item[4] else None,
-                    )
-                )
+
+                country_dict = {'country_name': row.get('Country'), 'country_code': row.get('ISO'), 'country': country}
+                cpi_data.append(ConsumerPriceIndex(**country_dict, **store))
             ConsumerPriceIndex.objects.bulk_create(cpi_data)
         self.stdout.write(self.style.SUCCESS('All done, bye!'))
