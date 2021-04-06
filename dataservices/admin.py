cc1a7b050985390440411ad3626570b5760fa287
Bob Meredith <bob.meredith@ntlworld.com>
2021-04-06 14:48:01 +0100

    GP2-1920_population_delete_strategy

--- dataservices/admin.py
+++ dataservices/admin.py
@@ -233,3 +233,33 @@ class TradingBlocsAdmin(admin.ModelAdmin):
     )
 
     list_filter = ('trading_bloc_name',)
+
+
+@admin.register(models.PopulationData)
+class PopulationDataAdmin(admin.ModelAdmin):
+    list_display = (
+        'country',
+        'year',
+        'gender',
+        'age_0_4',
+        'age_5_9',
+        'age_10_14',
+        'age_15_19',
+        'age_20_24',
+        'age_25_29',
+        'age_30_34',
+        'age_35_39',
+        'age_40_44',
+        'age_45_49',
+        'age_50_54',
+        'age_55_59',
+        'age_60_64',
+        'age_65_69',
+        'age_70_74',
+        'age_75_79',
+        'age_80_84',
+        'age_85_89',
+        'age_90_94',
+        'age_95_99',
+        'age_100_plus',
+    )
