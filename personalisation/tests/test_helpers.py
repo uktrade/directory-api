ee32efa8197d3adffc47f698e62d4d7abdb9ceac
Bob Meredith <bob.meredith@ntlworld.com>
2021-07-23 13:24:51 +0100

    Add helper tests

--- personalisation/tests/test_helpers.py
+++ personalisation/tests/test_helpers.py
@@ -1,6 +1,7 @@
+import pytest
 from unittest.mock import patch
 
-from personalisation import helpers
+from personalisation import helpers, models
 
 
 @patch('requests.get')
@@ -20,3 +21,28 @@ def test_exporting_is_great_handles_auth(mock_get, settings):
     )
     assert helpers.exopps_client.auth.username == username
     assert helpers.exopps_client.auth.password == password
+
+
+@pytest.mark.django_db
+def test_get_business_user():
+    business_user1 = helpers.get_business_user(6)
+    assert len(models.BusinessUser.objects.all()) == 1
+    business_user2 = helpers.get_business_user(6)
+    assert len(models.BusinessUser.objects.all()) == 1
+    assert business_user1 == business_user2
+
+
+@pytest.mark.django_db
+def test_create_or_update_product():
+    product_data1 = {'commodity_name': 'gin'}
+    product_data2 = {'commodity_name': 'cheese'}
+
+    user_id, user_product_id, user_product_data
+    helpers.create_or_update_product(user_id=1, user_product_data=product_data1)
+    business_user = helpers.get_business_user(1)
+    assert len(models.UserProduct.objects.all()) == 1
+    assert models.UserProduct.objects.get(business_user=business_user).product_data == product_data1
+    # Test temporary behaviour.  Adding a product, just updates the one existing one
+    helpers.create_or_update_product(user_id=1, user_product_data=product_data2)
+    assert len(models.UserProduct.objects.all()) == 1
+    assert models.UserProduct.objects.get(business_user=business_user).product_data == product_data2
