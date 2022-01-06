66f71ffb03cb3a8b6f60c6e4f841bdeb601bf725
Fred Marecesche <fred@froddd.co.uk>
2022-01-06 14:35:41 +0000

    Add extra commands to

--- dataservices/management/commands/import_all.py
+++ dataservices/management/commands/import_all.py
@@ -7,13 +7,17 @@ class Command(BaseCommand):
     help = 'Import all dataservices data'
 
     command_list = [
+        'import_countries',
         'import_cia_factbook_data',
         'import_cpi_data',
+        'import_currency_data',
         'import_population_urbanrural',
         'import_rank_of_law_data',
+        'import_suggested_countries',
         'import_target_age_groups',
         'import_trading_blocs',
-        'import_suggested_countries',
+        'import_weo_data',
+        'import_worldbank_data',
     ]
 
     def run_command(self, command, **options):
@@ -27,3 +31,5 @@ class Command(BaseCommand):
             self.run_command(command)
 
         self.stdout.write(self.style.SUCCESS('Import all data - Complete'))
+        self.stdout.write(self.style.WARNING('The ComTrade data import takes longer and has not been run. '
+                                             'Run it separately with the `import_comtrade_data` management command.'))
