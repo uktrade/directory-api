6da081c68855feb311139277d6ea69bc3d49a513
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-01-18 22:27:45 +0000

    wip

--- core/storage.py
+++ core/storage.py
@@ -5,7 +5,7 @@ from django.utils.functional import LazyObject
 
 class PrivateStorage(LazyObject):
     def _setup(self):
-        self._wrapped = get_storage_class(settings.STORAGES['private']['BACKEND'])()
+        self._wrapped = settings.STORAGES['private']['BACKEND']()
 
 
 private_storage = PrivateStorage()
