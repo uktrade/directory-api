5fff810ef1eb9d4177aa5c225cb6bffa51d85ad9
Stuart Mindt <stuart.mindt@madetech.com>
2024-07-17 09:40:52 +0100

    Added mgmt cmds for dbt sectors, gva bandings and invest opportunities. Exposed apis also

---
+++ dataservices/management/commands/import_dbt_investment_opportunities.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import sqlalchemy as sa
+
+from dataservices.models import DBTInvestmentOpportunity
+
+from .helpers import BaseDataWorkspaceIngestionCommand
+
+class Command(BaseDataWorkspaceIngestionCommand):
+    help = 'Import DBT investment opportunities data from Data Workspace'
+    sql = '''
+        SELECT
+            id,
+            updated_date,
+            investment_opportunity_code,
+            opportunity_title,
+            description,
+            nomination_round,
+            launched,
+            opportunity_type,
+            location,
+            sub_sector,
+            levelling_up,
+            net_zero,
+            science_technology_superpower,
+            sector_cluster
+        FROM public.dit_investment_opportunities
+    '''
+
+    def load_data(self):
+        data = []
+        chunks = pd.read_sql(sa.text(self.sql), self.engine, chunksize=5000)
+
+        for chunk in chunks:
+
+            for _idx, row in chunk.iterrows():
+                data.append(
+                    DBTInvestmentOpportunity(
+                        opportunity_title = row.opportunity_title,
+                        description = row.description,
+                        nomination_round = row.nomination_round,
+                        launched = row.launched,
+                        opportunity_type = row.opportunity_type,
+                        location = row.location,
+                        sub_sector = row.sub_sector,
+                        levelling_up = row.levelling_up,
+                        net_zero = row.net_zero,
+                        science_technology_superpower = row.science_technology_superpower,
+                        sector_cluster = row.sector_cluster,
+                    )
+                )
+
+        return data
---
+++ dataservices/management/commands/import_sectors_gva_value_bands.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import sqlalchemy as sa
+
+from dataservices.models import SectorGVAValueBand
+
+from .helpers import BaseDataWorkspaceIngestionCommand
+
+class Command(BaseDataWorkspaceIngestionCommand):
+    help = 'Import sector GVA value bands data from Data Workspace'
+    sql = '''
+        SELECT
+            id,
+            updated_date,
+            sector_gva_and_value_band_id,
+            full_sector_name,
+            gva_grouping,
+            gva_multiplier,
+            value_band_a_minimum,
+            value_band_b_minimum,
+            value_band_c_minimum,
+            value_band_d_minimum,
+            value_band_e_minimum,
+            sector_classification_value_band,
+            sector_classification_gva_multiplier,
+            start_date,
+            end_date
+        FROM public.ref_sectors_gva_value_bands
+    '''
+
+    def load_data(self):
+        data = []
+        chunks = pd.read_sql(sa.text(self.sql), self.engine, chunksize=5000)
+
+        for chunk in chunks:
+
+            for _idx, row in chunk.iterrows():
+                data.append(
+                    SectorGVAValueBand(
+                        full_sector_name = row.full_sector_name,
+                        value_band_a_minimum = row.value_band_a_minimum,
+                        value_band_b_minimum = row.value_band_b_minimum,
+                        value_band_c_minimum = row.value_band_c_minimum,
+                        value_band_d_minimum = row.value_band_d_minimum,
+                        value_band_e_minimum = row.value_band_e_minimum,
+                        start_date = row.start_date,
+                        end_date = row.end_date,
+                        sector_classification_value_band = row.sector_classification_value_band,
+                        sector_classification_gva_multiplier = row.sector_classification_gva_multiplier
+                    )
+                )
+
+        return data
