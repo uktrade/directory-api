bb4f1495426f1e06f782244df67640fc2cb5f1d9
Rafa Garcia <rafa@rafagarcia.info>
2024-07-31 14:21:48 +0100

    Initial implementation

--- core/management/commands/distributed_elasticsearch_migrate.py
+++ core/management/commands/distributed_elasticsearch_migrate.py
@@ -1,10 +1,9 @@
 from django.core.management.base import BaseCommand
 
 from core import tasks
-from core.management.commands import helpers
 
 
-class Command(helpers.ExclusiveDistributedHandleMixin, BaseCommand):
+class Command(BaseCommand):
     @staticmethod
     def is_migration_pending():
         # no harm done to he application if it stars before elsaticsearch
