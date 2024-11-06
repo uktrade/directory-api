035d6e5fe7a1d75b73e742f2b71d5e3cdab09fb1
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-11-06 16:25:05 +0000

    added gevent worker class

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: python manage.py distributed_migrate --noinput && python manage.py distributed_elasticsearch_migrate && gunicorn conf.wsgi:application --bind 0.0.0.0:$PORT
+web: python manage.py distributed_migrate --noinput && python manage.py distributed_elasticsearch_migrate && gunicorn conf.wsgi:application --config conf/gunicorn.py --bind 0.0.0.0:$PORT --worker-connections 1000
 celery_worker: python manage.py distributed_migrate --noinput && celery -A conf worker -l info
 celery_beat: python manage.py distributed_migrate --noinput && celery -A conf beat -l info -S django
---
+++ conf/tests/test_fork.py
@@ -0,0 +1,60 @@
+import pytest
+import os
+import logging
+from unittest.mock import patch
+from gevent.server import StreamServer
+from conf.gunicorn import post_fork
+from gunicorn.workers.ggevent import GeventWorker
+
+logger = logging.getLogger(__name__)
+
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+@pytest.fixture
+def worker():
+    my_dict = {
+        "max_requests": 1000,
+        "max_requests_jitter": 10,
+        "umask": 22,
+        "worker_tmp_dir": "/tmp",
+        "uid": os.geteuid(),
+        "gid": os.getegid(),
+        "worker_connections": 1000,
+    }
+    cfg_dict = dotdict(my_dict)
+
+    worker = GeventWorker(
+        log=logger,
+        age=1,
+        ppid=9999,
+        sockets=[],
+        app="directory_forms_api",
+        timeout=30,
+        cfg=cfg_dict,
+    )
+    return worker
+
+
+@pytest.fixture
+def server():
+    def handler():
+        return None
+
+    server = StreamServer(
+        listener=("0.0.0.0:8020"),
+        handle=handler,
+    )
+    return server
+
+
+@patch("conf.gunicorn.patch_with_psycogreen_gevent")
+def test_post_fork(mock_patch_with_psycogreen_gevent, worker, server):
+    post_fork(server, worker)
+    mock_patch_with_psycogreen_gevent.assert_called_once()
