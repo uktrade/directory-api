dcb8f91251ea242164e138f49e0cdd08695db38f
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-11-20 11:53:21 +0000

    moved test and this time saved all

--- dataservices/management/commands/tests/test_import_data.py
+++ dataservices/management/commands/tests/test_import_data.py
@@ -794,17 +794,7 @@ def test_comtrade_load_data(read_sql_mock):
     assert data_chn_wld.first().uk_or_world == 'WLD'
 
 
-@pytest.mark.django_db
-@mock.patch('dataservices.management.commands.helpers.save_dbt_sectors_data')
-@mock.patch('dataservices.management.commands.helpers.get_s3_file')
-@mock.patch('dataservices.management.commands.helpers.get_s3_paginator')
-def test_import_dbtsector_data_set_from_s3(
-    mock_get_s3_paginator, mock_get_s3_file, mock_save_dbt_sector_data, get_s3_file_data, get_s3_data_transfer_data
-):
-    mock_get_s3_file.return_value = get_s3_file_data
-    mock_get_s3_paginator.return_value = get_s3_data_transfer_data
-    management.call_command('import_dbt_sectors')
-    assert mock_save_dbt_sector_data.call_count == 1
+
 
 
 @pytest.mark.django_db
