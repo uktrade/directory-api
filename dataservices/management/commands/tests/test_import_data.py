30239c1a54969be64a987d20466abdd369c8ff45
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-11-20 11:12:47 +0000

    moved dataservices conftest.py

--- dataservices/management/commands/tests/test_import_data.py
+++ dataservices/management/commands/tests/test_import_data.py
@@ -795,10 +795,10 @@ def test_comtrade_load_data(read_sql_mock):
 
 
 @pytest.mark.django_db
-@mock.patch('dataservices.management.commands.helpers.save_dbt_sector_data')
+@mock.patch('dataservices.management.commands.helpers.save_dbt_sectors_data')
 @mock.patch('dataservices.management.commands.helpers.get_s3_file')
 @mock.patch('dataservices.management.commands.helpers.get_s3_paginator')
-def test_import_postcodes_data_set_from_s3(
+def test_import_dbtsector_data_set_from_s3(
     mock_get_s3_paginator, mock_get_s3_file, mock_save_dbt_sector_data, get_s3_file_data, get_s3_data_transfer_data
 ):
     mock_get_s3_file.return_value = get_s3_file_data
