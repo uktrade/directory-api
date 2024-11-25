f1c49643c27e03ca78d6ca73c8d08ef8e425bba8
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2024-11-25 16:05:49 +0000

    ADDED Dummy Env Vars

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -5,5 +5,18 @@ set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 
+export GOVNOTIFY_DUPLICATE_COMPANIES_EMAIL='info@great.gov.uk'
+export ACTIVITY_STREAM_INCOMING_IP_WHITELIST='abc123'
+export ACTIVITY_STREAM_OUTGOING_ACCESS_KEY='abc123'
+export ACTIVITY_STREAM_OUTGOING_SECRET_KEY='abc123'
+export ACTIVITY_STREAM_OUTGOING_URL='abc123'
+export ACTIVITY_STEAM_OUTGOING_IP_WHITELIST='abc123'
+export EXPORTING_OPPORTUNITIES_API_BASE_URL='abc123'
+export EXPORTING_OPPORTUNITIES_API_SECRET='abc123'
+export SOLE_TRADER_NUMBER_SEED='abc123'
+export CSV_DUMP_AUTH_TOKEN='abc123'
+export DATABASE_URL='postgres://localhost:6379'
+export REDIS_URL='redis://localhost:6379'
+
 echo "Running collectstatic"
 python manage.py collectstatic --noinput
