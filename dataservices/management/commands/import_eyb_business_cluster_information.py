36f97ccca687b8aa17c68be8f654d8d4cf57fbab
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-12-10 11:51:41 +0000

    Add Ingestion of Nomis Employee info

--- dataservices/management/commands/import_eyb_business_cluster_information.py
+++ dataservices/management/commands/import_eyb_business_cluster_information.py
@@ -1,15 +1,16 @@
 import json
+
 import pandas as pd
 import sqlalchemy as sa
-from sqlalchemy.ext.declarative import declarative_base
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from sqlalchemy.ext.declarative import declarative_base
 
 from dataservices.core.mixins import S3DownloadMixin
-from dataservices.management.commands.helpers import ingest_data, BaseDataWorkspaceIngestionCommand
+from dataservices.management.commands.helpers import ingest_data
 
 
-def get_uk_business_employee_counts_batch(data, data_table):
+def get_uk_business_employee_counts_tmp_batch(data, data_table):
 
     def get_table_data():
 
@@ -49,7 +50,49 @@ def get_uk_business_employee_counts_batch(data, data_table):
     )
 
 
-def get_uk_business_employee_counts_postgres_table(metadata, table_name):
+def get_uk_business_employee_counts_batch(data, data_table):
+
+    def get_table_data():
+
+        for uk_business_employee_count in data:
+            json_data = json.loads(uk_business_employee_count)
+
+            yield (
+                (
+                    data_table,
+                    (
+                        json_data['geo_description'],
+                        json_data['geo_code'],
+                        json_data['sic_code'],
+                        json_data['sic_description'],
+                        json_data['total_business_count'],
+                        json_data['business_count_release_year'],
+                        # missing employee data represented as np.nan which results in error saving django model
+                        # columns are int in dataframe so cannot store None resulting in below conditional assignment
+                        (
+                            json_data['total_employee_count']
+                            if json_data['total_employee_count'] and json_data['total_employee_count'] > 0
+                            else None
+                        ),
+                        (
+                            json_data['employee_count_release_year']
+                            if json_data['employee_count_release_year'] and json_data['employee_count_release_year'] > 0
+                            else None
+                        ),
+                        json_data['dbt_full_sector_name'],
+                        json_data['dbt_sector_name'],
+                    ),
+                )
+            )
+
+    return (
+        None,
+        None,
+        get_table_data(),
+    )
+
+
+def get_uk_business_employee_counts_postgres_tmp_table(metadata, table_name):
     return sa.Table(
         table_name,
         metadata,
@@ -66,12 +109,29 @@ def get_uk_business_employee_counts_postgres_table(metadata, table_name):
     )
 
 
+def get_uk_business_employee_counts_postgres_table(metadata, table_name):
+    return sa.Table(
+        table_name,
+        metadata,
+        sa.Column("geo_description", sa.TEXT, nullable=False),
+        sa.Column("geo_code", sa.TEXT, nullable=False),
+        sa.Column("sic_code", sa.TEXT, nullable=False),
+        sa.Column("sic_description", sa.TEXT, nullable=False),
+        sa.Column("total_business_count", sa.INTEGER, nullable=True),
+        sa.Column("business_count_release_year", sa.SMALLINT, nullable=True),
+        sa.Column("total_employee_count", sa.INTEGER, nullable=True),
+        sa.Column("employee_count_release_year", sa.SMALLINT, nullable=True),
+        sa.Column("dbt_full_sector_name", sa.TEXT, nullable=True),
+        sa.Column("dbt_sector_name", sa.TEXT, nullable=True),
+        schema="public",
+    )
+
+
 def get_sic_codes_dit_sector_mapping_batch(data, data_table):
 
     def get_table_data():
 
         for sic_codes_dit_sector_mapping in data:
-            breakpoint()
 
             json_data = json.loads(sic_codes_dit_sector_mapping)
 
@@ -105,19 +165,57 @@ def get_sic_codes_dit_sector_mapping_postgres_table(metadata, table_name):
     )
 
 
-def save_uk_business_employee_counts_data(data):
-    pass
-
-
-def save_uk_business_employee_counts_tmp_data(data):
-
-    table_name = 'dataservices_tmp_eybbusinessclusterinformation'
+def save_uk_business_employee_counts_data():
+    sql = """
+        SELECT
+            nubec.geo_description,
+            nubec.geo_code,
+            nubec.sic_code,
+            nubec.sic_description,
+            nubec.total_business_count,
+            nubec.business_count_release_year,
+            nubec.total_employee_count,
+            nubec.employee_count_release_year,
+            sector_mapping.dbt_full_sector_name,
+            sector_mapping.dbt_sector_name
+        FROM dataservices_tmp_eybbusinessclusterinformation nubec
+        LEFT JOIN (
+            SELECT
+                scmds.dbt_full_sector_name,
+                scmds.bt_sector_name,
+                scmds.five_digit_sic
+            from dataservices_tmp_sic_codes_dit_sector_mapping scmds
+        ) AS sector_mapping
+        ON nubec.sic_code = sector_mapping.five_digit_sic
+        WHERE nubec.geo_code <> 'K02000001'
+    """
 
     engine = sa.create_engine(settings.DATABASE_URL, future=True)
 
+    data = []
+
+    chunks = pd.read_sql(sa.text(sql), engine, chunksize=5000)
+
+    for chunk in chunks:
+        for _, row in chunk.iterrows():
+            data.append(
+                (
+                    row.geo_description,
+                    row.geo_code,
+                    row.sic_code,
+                    row.sic_description,
+                    row.total_business_count,
+                    row.business_count_release_year,
+                    row.total_employee_count,
+                    row.employee_count_release_year,
+                    row.dbt_full_sector_name,
+                    row.dbt_sector_name,
+                )
+            )
+
     metadata = sa.MetaData()
 
-    data_table = get_uk_business_employee_counts_postgres_table(metadata, table_name)
+    data_table = get_uk_business_employee_counts_postgres_table(metadata, 'dataservices_eybbusinessclusterinformation')
 
     def on_before_visible(conn, ingest_table, batch_metadata):
         pass
@@ -128,7 +226,26 @@ def save_uk_business_employee_counts_tmp_data(data):
     ingest_data(engine, metadata, on_before_visible, batches)
 
 
-def save_sic_codes_dit_sector_mapping_data(data):
+def save_uk_business_employee_counts_tmp_data(data):
+
+    table_name = 'dataservices_tmp_eybbusinessclusterinformation'
+
+    engine = sa.create_engine(settings.DATABASE_URL, future=True)
+
+    metadata = sa.MetaData()
+
+    data_table = get_uk_business_employee_counts_postgres_tmp_table(metadata, table_name)
+
+    def on_before_visible(conn, ingest_table, batch_metadata):
+        pass
+
+    def batches(_):
+        yield get_uk_business_employee_counts_tmp_batch(data, data_table)
+
+    ingest_data(engine, metadata, on_before_visible, batches)
+
+
+def save_sic_codes_dit_sector_mapping_tmp_data(data):
 
     table_name = 'dataservices_tmp_sic_codes_dit_sector_mapping'
 
@@ -158,43 +275,7 @@ def delete_temp_tables(table_names):
             Base.metadata.drop_all(engine, [table], checkfirst=True)
 
 
-def get_sic_codes_dit_sector_mapping():
-    sql = """
-        SELECT
-            scmds.dbt_full_sector_name,
-            scmds.dbt_sector_name,
-            -- necessary because sic codes are stored as integer in source table meaning leading 0 was dropped
-            substring(((scmds.sic_code + 100000)::varchar) from 2 for 5) as five_digit_sic
-        from public.ref_sic_codes_dit_sector_mapping scmds
-    """
-
-    data = []
-    engine = sa.create_engine(settings.DATABASE_URL, future=True)
-    chunks = pd.read_sql(sa.text(sql), engine, chunksize=5000)
-
-    for chunk in chunks:
-        for _, row in chunk.iterrows():
-            data.append(
-                (
-                    row.geo_description,
-                    row.geo_code,
-                    row.sic_code,
-                    row.sic_description,
-                    row.total_business_count,
-                    row.business_count_release_year,
-                    # missing employee data represented as np.nan which results in error saving django model
-                    # columns are int in dataframe so cannot store None resulting in below conditional assignment
-                    row.total_employee_count if row.total_employee_count > 0 else None,
-                    row.employee_count_release_year if row.employee_count_release_year > 0 else None,
-                    row.dbt_full_sector_name,
-                    row.dbt_sector_name,
-                )
-            )
-
-    return data
-
-
-class Command(BaseCommand, S3DownloadMixin, BaseDataWorkspaceIngestionCommand):
+class Command(BaseCommand, S3DownloadMixin):
 
     help = 'Import ONS total UK business and employee counts per region and section, 2 and 5 digit Standard Industrial Classification'  # noqa:E501
 
@@ -203,38 +284,43 @@ class Command(BaseCommand, S3DownloadMixin, BaseDataWorkspaceIngestionCommand):
             prefix=settings.NOMIS_UK_BUSINESS_EMPLOYEE_COUNTS_FROM_S3_PREFIX,
             save_func=save_uk_business_employee_counts_tmp_data,
         )
-        data = get_sic_codes_dit_sector_mapping()
-        save_uk_business_employee_counts_data(data)
+        sic_code_data = self.load_sic_code_data()
+        save_sic_codes_dit_sector_mapping_tmp_data(sic_code_data)
+        save_uk_business_employee_counts_data()
 
-        delete_temp_tables(['dataservices_tmp_eybbusinessclusterinformation', 'dataservices_tmp_sic_codes_dit_sector_mapping',])
-        table_name = 'dataservices_tmp_sic_codes_dit_sector_mapping'
-       
-    def load_data(self):
-# #         data = []
-# #         chunks = pd.read_sql(sa.text(self.sql), self.engine, chunksize=5000)
+        delete_temp_tables(
+            [
+                'dataservices_tmp_eybbusinessclusterinformation',
+                'dataservices_tmp_sic_codes_dit_sector_mapping',
+            ]
+        )
 
-# #         for chunk in chunks:
-# #             for _idx, row in chunk.iterrows():
-# #                 data.append(
-# #                     EYBBusinessClusterInformation(
-# #                         geo_description=row.geo_description,
-# #                         geo_code=row.geo_code,
-# #                         sic_code=row.sic_code,
-# #                         sic_description=row.sic_description,
-# #                         total_business_count=row.total_business_count,
-# #                         business_count_release_year=row.business_count_release_year,
-# #                         # missing employee data represented as np.nan which results in error saving django model
-# #                         # columns are int in dataframe so cannot store None resulting in below conditional assignment
-# #                         total_employee_count=row.total_employee_count if row.total_employee_count > 0 else None,
-# #                         employee_count_release_year=(
-# #                             row.employee_count_release_year if row.employee_count_release_year > 0 else None
-# #                         ),
-# #                         dbt_full_sector_name=row.dbt_full_sector_name,
-# #                         dbt_sector_name=row.dbt_sector_name,
-# #                     )
-# #                 )
+    def load_sic_code_data(self):
+        sql = """
+            SELECT
+                scmds."DIT full sector name" as dbt_full_sector_name,
+                scmds."DIT sector" as dbt_sector_name,
+                -- necessary because sic codes are stored as integer in source table meaning leading 0 was dropped
+                substring(((scmds."SIC code" + 100000)::varchar) from 2 for 5) as five_digit_sic
+            from public.ref_sic_codes_dit_sector_mapping scmds
+        """
 
-# #         return data
+        data = []
+        engine = sa.create_engine(settings.DATA_WORKSPACE_DATASETS_URL, execution_options={'stream_results': True})
+        chunks = pd.read_sql(sa.text(sql), engine, chunksize=5000)
+
+        for chunk in chunks:
+            for _, row in chunk.iterrows():
+                data.append(
+                    (
+                        row.dbt_full_sector_name,
+                        row.dbt_sector_name,
+                        row.five_digit_sic,
+                    )
+                )
+
+        return data
+
 
 # class Command(BaseDataWorkspaceIngestionCommand):
 #     help = 'Import ONS total UK business and employee counts per region and section, 2 and 5 digit Standard Industrial Classification'  # noqa:E501
