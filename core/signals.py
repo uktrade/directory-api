47ea96532e813c0cf2d9aa6bebc3fb5dd04b6178
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-12-10 09:09:35 +0000

    update comment

--- core/signals.py
+++ core/signals.py
@@ -2,7 +2,7 @@ import logging
 
 from django.conf import settings
 from django.core.cache import cache
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 logger = logging.getLogger(__name__)
@@ -14,7 +14,7 @@ def clear_the_cache(**kwargs):
     '''
     We have implemented Django caching middleware. By default, this caches all GET requests.
 
-    This signal will invalidate the cache when any models.Model.save() method is called.
+    This signal will invalidate the cache when any models.Model.save() and delete() method is called.
     '''
     if settings.DEBUG:
         logger.info('A models.Model.save() has been invoked - attempting cache.clear() call.')
