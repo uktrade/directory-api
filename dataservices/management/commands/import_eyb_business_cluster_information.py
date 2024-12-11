9287dfb6248f6ed5ee6656797f3f1fd1013eb149
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-12-11 11:13:23 +0000

    Ingest nomis UK employee data from s3

--- dataservices/management/commands/import_eyb_business_cluster_information.py
+++ dataservices/management/commands/import_eyb_business_cluster_information.py
@@ -59,6 +59,9 @@ def get_uk_business_employee_counts_batch(data, data_table):
 
         for json_data in data:
 
+            if json_data['geo_code'] == 'K02000001':
+                continue
+
             yield (
                 (
                     data_table,
@@ -187,7 +190,7 @@ def get_ref_sic_codes_mapping_postgres_table(metadata, table_name):
         table_name,
         metadata,
         sa.Column("id", sa.INTEGER, nullable=False),
-        sa.Column("sic_code", sa.TEXT, nullable=False),
+        sa.Column("sic_code", sa.INTEGER, nullable=False),
         sa.Column("mapping_id", sa.TEXT, nullable=True),
         sa.Column("dit_sector_list_id", sa.INTEGER, nullable=True),
         sa.Index(None, "dit_sector_list_id"),
@@ -221,16 +224,16 @@ def save_uk_business_employee_counts_data():
             nubec.employee_count_release_year,
             sector_mapping.dbt_full_sector_name,
             sector_mapping.dbt_sector_name
-        FROM dataservices_tmp_eybbusinessclusterinformation nubec
+        FROM public.dataservices_tmp_eybbusinessclusterinformation nubec
           LEFT JOIN (
-            SELECT
-                sectorref.full_sector_name as dbt_full_sector_name,
-                sectorref.field_04 as dbt_sector_name,
-                substring(((dataservices_tmp_ref_sic_codes_mapping.sic_code + 100000)::varchar) from 2 for 5) as five_digit_sic  # noqa:E501
-            from dataservices_tmp_sector_reference sectorref
-            INNER JOIN dataservices_tmp_ref_sic_codes_mapping ON dataservices_tmp_ref_sic_codes_mapping.dit_sector_list_id = dataservices_tmp_sector_reference.id
+              SELECT
+                dataservices_tmp_sector_reference.full_sector_name as dbt_full_sector_name,
+                dataservices_tmp_sector_reference.field_04 as dbt_sector_name,
+                -- necessary because sic codes are stored as integer in source table meaning leading 0 was dropped
+                substring(((dataservices_tmp_ref_sic_codes_mapping.sic_code + 100000)::varchar) from 2 for 5) as five_digit_sic  --   # noqa:E501
+            FROM public.dataservices_tmp_ref_sic_codes_mapping
+            INNER JOIN public.dataservices_tmp_sector_reference ON public.dataservices_tmp_ref_sic_codes_mapping.dit_sector_list_id = public.dataservices_tmp_sector_reference.id
           ) as sector_mapping
-        WHERE nubec.geo_code <> 'K02000001'
         ON nubec.sic_code = sector_mapping.five_digit_sic
     """
 
@@ -238,7 +241,6 @@ def save_uk_business_employee_counts_data():
 
     data = []
 
-    breakpoint()
     with engine.connect() as connection:
         chunks = pd.read_sql_query(sa.text(sql), connection, chunksize=5000)
 
@@ -291,7 +293,7 @@ def save_uk_business_employee_counts_tmp_data(data):
     ingest_data(engine, metadata, on_before_visible, batches)
 
 
-def save_ref_sic_codes_mapping_tmp_data(data):
+def save_ref_sic_codes_mapping_data(data):
 
     table_name = 'dataservices_tmp_ref_sic_codes_mapping'
 
@@ -310,7 +312,7 @@ def save_ref_sic_codes_mapping_tmp_data(data):
     ingest_data(engine, metadata, on_before_visible, batches)
 
 
-def save_sector_reference_dataset_tmp_data(data):
+def save_sector_reference_dataset_data(data):
 
     table_name = 'dataservices_tmp_sector_reference'
 
@@ -353,11 +355,11 @@ class Command(BaseCommand, S3DownloadMixin):
             )
             self.do_handle(
                 prefix=settings.REF_SIC_CODES_MAPPING_FROM_S3_PREFIX,
-                save_func=save_ref_sic_codes_mapping_tmp_data,
+                save_func=save_ref_sic_codes_mapping_data,
             )
             self.do_handle(
                 prefix=settings.SECTOR_REFERENCE_DATASET_FROM_S3_PREFIX,
-                save_func=save_sector_reference_dataset_tmp_data,
+                save_func=save_sector_reference_dataset_data,
             )
             save_uk_business_employee_counts_data()
         except Exception:
@@ -370,59 +372,3 @@ class Command(BaseCommand, S3DownloadMixin):
                     'dataservices_tmp_sector_reference',
                 ]
             )
-
-
-# class Command(BaseDataWorkspaceIngestionCommand):
-#     help = 'Import ONS total UK business and employee counts per region and section, 2 and 5 digit Standard Industrial Classification'  # noqa:E501
-
-#     sql = '''
-#         SELECT
-#             nubec.geo_description,
-#             nubec.geo_code,
-#             nubec.sic_code,
-#             nubec.sic_description,
-#             nubec.total_business_count,
-#             nubec.business_count_release_year,
-#             nubec.total_employee_count,
-#             nubec.employee_count_release_year,
-#             sector_mapping.dbt_full_sector_name,
-#             sector_mapping.dbt_sector_name
-#         FROM ons.nomis__uk_business_employee_counts nubec
-#         LEFT JOIN (
-#             SELECT
-#                 scmds."DIT full sector name" as dbt_full_sector_name,
-#                 scmds."DIT sector" as dbt_sector_name,
-#                 -- necessary because sic codes are stored as integer in source table meaning leading 0 was dropped
-#                 substring(((scmds."SIC code" + 100000)::varchar) from 2 for 5) as five_digit_sic
-#             from public.ref_sic_codes_dit_sector_mapping scmds
-#         ) AS sector_mapping
-#         ON nubec.sic_code = sector_mapping.five_digit_sic
-#         WHERE nubec.geo_code <> 'K02000001'
-#     '''
-
-#     def load_data(self):
-#         data = []
-#         chunks = pd.read_sql(sa.text(self.sql), self.engine, chunksize=5000)
-
-#         for chunk in chunks:
-#             for _idx, row in chunk.iterrows():
-#                 data.append(
-#                     EYBBusinessClusterInformation(
-#                         geo_description=row.geo_description,
-#                         geo_code=row.geo_code,
-#                         sic_code=row.sic_code,
-#                         sic_description=row.sic_description,
-#                         total_business_count=row.total_business_count,
-#                         business_count_release_year=row.business_count_release_year,
-#                         # missing employee data represented as np.nan which results in error saving django model
-#                         # columns are int in dataframe so cannot store None resulting in below conditional assignment
-#                         total_employee_count=row.total_employee_count if row.total_employee_count > 0 else None,
-#                         employee_count_release_year=(
-#                             row.employee_count_release_year if row.employee_count_release_year > 0 else None
-#                         ),
-#                         dbt_full_sector_name=row.dbt_full_sector_name,
-#                         dbt_sector_name=row.dbt_sector_name,
-#                     )
-#                 )
-
-#         return data
