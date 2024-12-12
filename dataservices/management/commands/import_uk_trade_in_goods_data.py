bacd390cb0038b56fecc523c301fe13334ff8c4d
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-12-12 22:51:13 +0000

    refactor save data

--- dataservices/management/commands/import_uk_trade_in_goods_data.py
+++ dataservices/management/commands/import_uk_trade_in_goods_data.py
@@ -1,7 +1,6 @@
 import json
 import logging
 
-import pandas as pd
 import sqlalchemy as sa
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -11,6 +10,9 @@ from dataservices.management.commands.helpers import ingest_data
 from dataservices.models import Country
 
 logger = logging.getLogger(__name__)
+
+TEMP_TABLE = 'dataservices_tmp_uktradeingoodsbycountry'
+LIVE_TABLE = 'dataservices_uktradeingoodsbycountry'
 
 
 def get_uk_trade_in_goods_tmp_batch(data, data_table):
@@ -82,7 +84,7 @@ def get_uk_trade_in_goods_tmp_batch(data, data_table):
 
 def get_uk_trade_in_goods_tmp_postgres_table(metadata):
     return sa.Table(
-        "dataservices_tmp_uktradeingoodsbycountry",
+        TEMP_TABLE,
         metadata,
         sa.Column("ons_iso_alpha_2_code", sa.TEXT, nullable=False),
         sa.Column("period", sa.TEXT, nullable=False),
@@ -90,13 +92,18 @@ def get_uk_trade_in_goods_tmp_postgres_table(metadata):
         sa.Column("product_name", sa.TEXT, nullable=False),
         sa.Column("imports", sa.DECIMAL(10, 2), nullable=True),
         sa.Column("exports", sa.DECIMAL(10, 2), nullable=True),
+        sa.Index(None, "ons_iso_alpha_2_code"),
+        sa.Index(None, "period"),
+        sa.Index(None, "product_code"),
+        sa.Index(None, "product_name"),
+        sa.Index(None, "ons_iso_alpha_2_code", "period", "product_code", "product_name"),
         schema="public",
     )
 
 
 def get_uk_trade_in_goods_postgres_table(metadata):
     return sa.Table(
-        "dataservices_uktradeingoodsbycountry",
+        LIVE_TABLE,
         metadata,
         sa.Column("country_id", sa.INTEGER, nullable=False),
         sa.Column("year", sa.INTEGER, nullable=False),
@@ -157,6 +164,7 @@ def save_uk_trade_in_goods_tmp_data(data):
 def save_uk_trade_in_goods_data():
 
     engine = sa.create_engine(settings.DATABASE_URL, future=True)
+
     data = []
 
     sql = '''
@@ -176,10 +184,15 @@ def save_uk_trade_in_goods_data():
     '''
 
     with engine.connect() as connection:
-        chunks = pd.read_sql_query(sa.text(sql), connection, chunksize=10000)
 
-        for chunk in chunks:
-            for _, row in chunk.iterrows():
+        cnt = 0
+        while True:
+            batch = connection.execute(sa.text(sql)).fetchmany(100000)
+
+            if not batch:
+                break
+
+            for row in batch:
 
                 try:
                     country = Country.objects.get(iso2=row.iso2)
@@ -202,9 +215,13 @@ def save_uk_trade_in_goods_data():
                     }
                 )
 
+                cnt += 1
+                if cnt % 100000:
+                    logger.info(f'Processing record {cnt}')
+
     metadata = sa.MetaData()
 
-    data_table = get_uk_trade_in_goods_postgres_table(metadata, 'dataservices_eybbusinessclusterinformation')
+    data_table = get_uk_trade_in_goods_postgres_table(metadata)
 
     def on_before_visible(conn, ingest_table, batch_metadata):
         pass
@@ -226,77 +243,12 @@ class Command(BaseCommand, S3DownloadMixin):
                 save_func=save_uk_trade_in_goods_tmp_data,
             )
             save_uk_trade_in_goods_data()
+            logger.info("import_uk_trade_in_goods_data completed successfully")
         except Exception:
             logger.exception("import_uk_trade_in_goods_data failed to ingest data from s3")
         finally:
             self.delete_temp_tables(
                 [
-                    'dataservices_tmp_uktradeingoodsbycountry',
+                    TEMP_TABLE,
                 ]
             )
-
-
-# class Command(MarketGuidesDataIngestionCommand):
-#     help = 'Import ONS UK trade in goods data by country from Data Workspace'
-
-#     sql = '''
-#         SELECT
-#             iso2,
-#             period,
-#             commodity_code,
-#             commodity_name,
-#             sum(imports) AS imports,
-#             sum(exports) AS exports
-#         FROM (
-#             SELECT
-#                 ons_iso_alpha_2_code AS iso2,
-#                 period,
-#                 product_code AS commodity_code,
-#                 product_name AS commodity_name,
-#                 CASE WHEN direction = 'imports' THEN
-#                     value
-#                 END AS imports,
-#                 CASE WHEN direction = 'exports' THEN
-#                     value
-#                 END AS exports
-#             FROM
-#                 ons.trade__uk_goods_nsa
-#             WHERE
-#                 period_type = 'quarter'
-#                 AND product_code NOT IN('0', '1', '2', '3', '33', '3OF', '4', '5', '6', '7', '78', '79',
-#                 '792/3', '7E', '7EI', '7EK', '7M', '7MC', '7MI', '7MK', '8', '8O', '8OC', 'T')) s
-#         GROUP BY
-#             iso2,
-#             period,
-#             commodity_code,
-#             commodity_name;
-#     '''
-
-#     def load_data(self):
-#         data = []
-#         chunks = pd.read_sql(sa.text(self.sql), self.engine, chunksize=10000)
-
-#         for chunk in chunks:
-#             for _idx, row in chunk.iterrows():
-#                 try:
-#                     country = Country.objects.get(iso2=row.iso2)
-#                 except Country.DoesNotExist:
-#                     continue
-
-#                 year, quarter = row.period.replace('quarter/', '').split('-Q')
-#                 imports = None if row.imports != row.imports else row.imports
-#                 exports = None if row.exports != row.exports else row.exports
-
-#                 data.append(
-#                     UKTradeInGoodsByCountry(
-#                         country=country,
-#                         year=year,
-#                         quarter=quarter,
-#                         commodity_code=row.commodity_code,
-#                         commodity_name=row.commodity_name,
-#                         imports=imports,
-#                         exports=exports,
-#                     )
-#                 )
-
-#         return data
