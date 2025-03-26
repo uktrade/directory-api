7bff7871b1e973c3a658279061e2bde02689a141
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-03-26 16:59:54 +0000

    Fix failing test

--- company/tests/test_tasks.py
+++ company/tests/test_tasks.py
@@ -3,6 +3,7 @@ from unittest import mock
 import pytest
 
 from company import tasks
+from company.tests.factories import CompanyUserFactory
 
 
 @mock.patch('company.tasks.lock_acquired', mock.Mock(return_value=True))
@@ -22,5 +23,6 @@ def test_suppliers_csv_upload(mocked_call_command):
 @mock.patch('company.management.commands.obsfucate_personal_details.Command.mask_company_user')
 @pytest.mark.django_db
 def test_obsfucate_personal_details(mock_mask_company_user):
+    CompanyUserFactory()
     tasks.obsfucate_personal_details()
     assert mock_mask_company_user.call_count == 1
